9fc06fe92f2087b8b96acfc7e8f05d4a90a07258
Rémi Palancher <remi@rezib.org>
2015-04-09 11:46:14 +0200

    Set doc latexpdf format to a4paper

--- doc/conf.py
+++ doc/conf.py
@@ -189,7 +189,7 @@ htmlhelp_basename = 'Slurm-webdoc'
 
 latex_elements = {
 # The paper size ('letterpaper' or 'a4paper').
-#'papersize': 'letterpaper',
+'papersize': 'a4paper',
 
 # The font size ('10pt', '11pt' or '12pt').
 #'pointsize': '10pt',
