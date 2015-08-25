da863da2e7f2fb7118b758b7fe76499ca03490b7
Lorel <lorel@wellatribe.com>
2015-08-25 13:35:14 +0200

    authentication with LDAP #48

--- rest/auth.py
+++ rest/auth.py
@@ -1,8 +1,7 @@
+import ldap
 from flask import request, abort, jsonify
 from settings import settings
 from functools import wraps
-
-# for authentication
 from itsdangerous import (TimedJSONWebSignatureSerializer
                           as Serializer, BadSignature, SignatureExpired)
 
@@ -14,6 +13,11 @@ filtered_keys_by_role = {
     'admin': []
 }
 unauthorized_roles = ['all']
+
+
+def get_ldap_connection():
+    conn = ldap.initialize(settings.get('ldap', 'uri'))
+    return conn
 
 
 def filter_dict(to_filter={}, filtered_keys=[]):
@@ -29,13 +33,61 @@ class User(object):
     def __init__(self, username, password):
         self.username = username
         self.password = password
+
         # here deal with ldap to get user role
-        self.role = 'admin'
+        conn = get_ldap_connection()
+
+        try:
+            # authicate user on ldap
+            conn.simple_bind_s(
+                'uid=%s,ou=%s,%s' % (
+                    username,
+                    settings.get('ldap', 'ugroup'),
+                    settings.get('ldap', 'base')
+                ),
+                password
+            )
+
+            print "User %s authenticated" % username
+
+            # retrieve user's group id
+            results = conn.search_s(
+                'uid=%s,ou=%s,%s' % (
+                    username,
+                    settings.get('ldap', 'ugroup'),
+                    settings.get('ldap', 'base')
+                ),
+                ldap.SCOPE_SUBTREE
+            )
+            print results
+            gidNumber = results[0][1]['gidNumber'][0]
+
+            print "User's group number: %s" % gidNumber
+
+            # retrieve user's group name
+            filter = 'gidNumber=%s' % gidNumber
+            results = conn.search_s(
+                'ou=groups,%s' % settings.get('ldap', 'base'),
+                ldap.SCOPE_SUBTREE,
+                filter
+            )
+            print results
+            gName = results[0][1]['cn'][0]
+
+            print "User's group name: %s" % gName
+
+            self.role = 'admin'
+
+        except ldap.INVALID_CREDENTIALS:
+            print "Authentication failed: username or password is incorrect."
+            return None
 
     def generate_auth_token(self, expiration=600):
         s = Serializer(secret_key, expires_in=expiration)
-        token = s.dumps({'username': self.username,
-                         'password': self.password})
+        token = s.dumps({
+            'username': self.username,
+            'password': self.password
+        })
         print "generate_auth_token : token -> %s" % token
         return token
 
@@ -62,8 +114,9 @@ def authentication_verify():
             token = request.json['token']
             user = User.verify_auth_token(token)
             if user is not None:
-                if user.role not in (set(filtered_keys_by_role) -
-                                     set(unauthorized_roles)):
+                if user.role not in (
+                    set(filtered_keys_by_role) - set(unauthorized_roles)
+                ):
                     return abort(403)
 
                 resp = f(*args, **kwargs)
