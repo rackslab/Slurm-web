854d9fafee51fecd1b832bf4aafe4b35aa95535b
Fabien Gavory <fabien@gavory.fr>
2015-08-28 15:18:10 +0200

    add auth

--- js/utils/token.js
+++ js/utils/token.js
@@ -1,8 +1,7 @@
 define([], function () {
   return {
     getToken: function () {
-      return 'eyJhbGciOiJIUzI1NiIsImV4cCI6MTQ0MjA0NDA5OSwiaWF0IjoxNDQwNzQ4MDk5fQ.eyJ1c2VybmFtZSI6IioiLCJwYXNzd29yZCI6IioifQ.MpdQVjhaLMvLaow0G1AkDqsozSjXtMPhQSpKmsx4dxo';
-      //return localStorage.getItem('jwt');
+      return localStorage.getItem('jwt');
     },
     setToken: function (token) {
       localStorage.setItem('jwt', token)
