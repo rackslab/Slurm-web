7d44eae98d641396a2ba7a1527855cc87390dd2a
Fabien Gavory <fabien@gavory.fr>
2015-08-31 17:59:06 +0200

    add guest auth

--- js/utils/ajax.js
+++ js/utils/ajax.js
@@ -1,5 +1,6 @@
-define(['jquery', 'token-utils', 'user-utils'], function (token, user) {
+define(['jquery', 'token-utils', 'user-utils'], function ($, token, user) {
   $(document).ajaxError(function (event, jqueryXHR, error, errorThrown) {
+    console.log(error)
     if (jqueryXHR.status && (jqueryXHR.status === 403)) {
       token.removeToken();
       user.removeUser();
