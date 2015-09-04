8e2fc5bdfe65546c6f060059975b1f2103223a57
Fabien Gavory <fabien@gavory.fr>
2015-09-04 17:30:35 +0200

    add intersections 3D view

--- js/utils/draw-three-dimensional.js
+++ js/utils/draw-three-dimensional.js
@@ -111,6 +111,9 @@ THREE.FirstPersonControls = function ( object, domElement ) {
 
     if ( this.domElement === document ) {
 
+      this.pointerX = (event.clientX / window.innerWidth) * 2 - 1;
+      this.pointerY = -(event.clientY / window.innerHeight) * 2 + 1;
+
       this.mouseX = event.pageX - this.viewHalfX;
       this.mouseY = event.pageY - this.viewHalfY;
 
@@ -256,7 +259,6 @@ THREE.FirstPersonControls = function ( object, domElement ) {
 
   };
 
-
   this.domElement.addEventListener( 'contextmenu', function ( event ) { event.preventDefault(); }, false );
 
   this.domElement.addEventListener( 'mousemove', bind( this, this.onMouseMove ), false );
@@ -279,9 +281,10 @@ THREE.FirstPersonControls = function ( object, domElement ) {
 
   return function () {
     /* Later, put it in config */
-    var MOVESPEED = 100;
-    var LOOKSPEED = 0.075;
-    var UNITSIZE = 250;
+    var MOVESPEED = 1;
+    var LOOKSPEED = 0.05;
+    var UNITSIZE = 1;
+    var WALLHEIGHT = UNITSIZE / 3;
     /**/
     var self = this;
     this.map = [
@@ -304,8 +307,33 @@ THREE.FirstPersonControls = function ( object, domElement ) {
 
     function onMouseMove(e) {
       e.preventDefault();
-      self.mouse.x = (e.clientX / self.canvas.width()) * 2 - 1;
-      self.mouse.y = - (e.clientY / self.canvas.height()) * 2 + 1;
+
+      //self.mouse.x = (e.clientX / self.canvas.width()) * 2 - 1;
+      //self.mouse.y = -(e.clientY / self.canvas.height()) * 2 + 1;
+    }
+
+    function onMouseDown(evenet) {
+      event.preventDefault();
+ 
+
+       var vector = new THREE.Vector3();
+
+var canvasMouseX = event.clientX - self.canvasRect.left; 
+var canvasMouseY = event.clientY - self.canvasRect.top; 
+vector.set((canvasMouseX / $('canvas').width()) * 2 - 1, -(canvasMouseY / $('canvas').height()) * 2 + 1, 0.5);
+
+
+      vector.unproject( self.camera );
+
+      self.raycaster.set( self.camera.position, vector.sub( self.camera.position ).normalize() );
+
+      //self.raycaster.setFromCamera( vector, self.camera );
+
+      var intersects = self.raycaster.intersectObjects( self.scene.children, true );
+      console.log(intersects)
+      for ( var i = 0; i < intersects.length; i++ ) {
+        //intersects[ i ].object.material.color.set( 0xff0000 );
+      }
     }
 
     function setControls() {
@@ -315,27 +343,59 @@ THREE.FirstPersonControls = function ( object, domElement ) {
       self.controls.lookSpeed = LOOKSPEED;
       self.controls.lookVertical = true;
       self.controls.noFly = true;
-      $(self.canvas).on('mousemove', onMouseMove);
+      //$(self.canvas).on('mousemove', onMouseMove);
+      $(self.canvas).on('mousedown', onMouseDown);
     }
 
     function addLight() {
-      var directionalLight1 = new THREE.DirectionalLight( 0xF7EFBE, 0.7 );
+      var directionalLight1 = new THREE.DirectionalLight( 0xF7EFBE, 1 );
       directionalLight1.position.set( 0.5, 1, 0.5 );
       self.scene.add( directionalLight1 );
-      var directionalLight2 = new THREE.DirectionalLight( 0xF7EFBE, 0.5 );
+      var directionalLight2 = new THREE.DirectionalLight( 0xF7EFBE, 1 );
       directionalLight2.position.set( -0.5, -1, -0.5 );
       self.scene.add( directionalLight2 );
     }
 
-    function addScene() {
-      var floorCube = new THREE.BoxGeometry(self.mapWidth * UNITSIZE, 10, self.mapHeight * UNITSIZE);
-      var floorMaterial = new THREE.MeshLambertMaterial({color: 0xEDCBA0,/*map: THREE.ImageUtils.loadTexture('images/floor.jpg')*/})
-      var floor = new THREE.Mesh(floorCube, floorMaterial);
+    function addFloor() {
+      var texture = THREE.ImageUtils.loadTexture('static/floor.jpg');
+      texture.wrapS = THREE.RepeatWrapping;
+      texture.wrapT = THREE.RepeatWrapping;
+      texture.repeat.set( 32, 32 );
+      var floorMaterial = new THREE.MeshBasicMaterial({ map: texture, side: THREE.DoubleSide });
+      var floorGeometry = new THREE.PlaneGeometry(self.mapWidth * UNITSIZE, self.mapHeight * UNITSIZE, 100, 100);
+      var floor = new THREE.Mesh(floorGeometry, floorMaterial);
+      floor.rotation.x = (-Math.PI / 2);
+      console.log(floor);
       self.scene.add(floor);
     }
 
-    function addCamera(canvas) {
-      self.camera = new THREE.PerspectiveCamera(60, canvas.width() / canvas.height(), 1, 10000);
+    function addWall() {
+      var texture = THREE.ImageUtils.loadTexture('static/floor.jpg');
+      texture.wrapS = THREE.RepeatWrapping;
+      texture.wrapT = THREE.RepeatWrapping;
+      texture.repeat.set( self.mapWidth, self.mapHeight );
+
+      var cube = new THREE.CubeGeometry(UNITSIZE, UNITSIZE, UNITSIZE);
+      var materials = new THREE.MeshBasicMaterial({ map: texture, side: THREE.DoubleSide });
+      for (var i = 0; i < self.mapWidth; i++) {
+        for (var j = 0, m = self.map[i].length; j < m; j++) {
+          if (self.map[i][j] === 1) {
+            var wall = new THREE.Mesh(cube, materials);
+            wall.position.x = (i - self.mapWidth / 2) * UNITSIZE;
+            wall.position.y = WALLHEIGHT / 2;
+            wall.position.z = (j - self.mapWidth / 2) * UNITSIZE;
+            self.scene.add(wall);
+          }
+        }
+      }
+    }
+
+    function addRack() {
+
+    }
+
+    function addCamera() {
+      self.camera = new THREE.PerspectiveCamera(60, self.canvas.width() / self.canvas.height(), 1, 10000);
       self.scene.add(self.camera);
     }
 
@@ -350,17 +410,30 @@ THREE.FirstPersonControls = function ( object, domElement ) {
     this.init = function (canvas) {
       this.canvas = canvas;
       this.clock = new THREE.Clock();
+      this.mouse = new THREE.Vector2();
+      this.raycaster = new THREE.Raycaster();
+      console.log(this.raycaster.near)
+      console.log(this.raycaster.far)
+      //this.raycaster.near = 1000000;
+      //this.raycaster.far = 1000000;
       this.renderer = new THREE.WebGLRenderer({ canvas: canvas[0] });
       this.scene = new THREE.Scene();
 
+
+      this.canvasRect = this.renderer.domElement.getBoundingClientRect();
+
       addCamera(canvas);
       setControls();
-      addScene();
+      //addFloor();
 
       var geometry = new THREE.BoxGeometry( 1, 1, 1 );
       var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
       this.cube = new THREE.Mesh( geometry, material );
-      this.scene.add( this.cube );
+      this.cube.custom = {
+        type: 'cube'
+      };
+      this.cube.position.y = 0.5;
+      this.scene.add(this.cube);
 
       this.camera.position.z = 5;
 
