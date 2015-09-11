b62e25d46a15499395edc337a4c75bb0b47f98d0
Fabien Gavory <fabien@gavory.fr>
2015-09-11 16:12:09 +0200

    3d view

---
+++ js/utils/draw-colors.js
@@ -0,0 +1,42 @@
+define(['text!../config/3d.colors.config.json'], function (colors) {
+  colors = JSON.parse(colors);
+
+  return {
+    findJobColor: function (jobId) {
+      return colors.JOB[(jobId % colors.JOB.length)];
+    },
+    findLEDColor: function (node) {
+      var color;
+
+      switch(node.node_state) {
+        case 'IDLE':
+        case 'IDLE*':
+          color = colors.LED.IDLE;
+          break;
+        case 'ALLOCATED':
+        case 'ALLOCATED*':
+        case 'COMPLETING':
+        case 'RESERVED':
+        case 'COMPLETING*':
+          if (node.total_cpus === -node.cpus) {
+            color = colors.LED.FULLYALLOCATED;
+            break;
+          }
+          color = colors.LED.PARTALLOCATED;
+          break;
+        case 'DRAINING':
+        case 'DRAINING*':
+        case 'DRAINED':
+        case 'DRAINED*':
+        case 'DOWN':
+        case 'DOWN*':
+          color = colors.LED.UNAVAILABLE;
+          break;
+        default:
+          color = colors.LED.UNKNOWN;
+      }
+
+      return color;
+    }
+  };
+});
--- js/utils/racks.js
+++ js/utils/racks.js
@@ -1,4 +1,4 @@
-define(['text!config.json'], function (config) {
+define(['text!../config/3d.config.json'], function (config) {
   config = JSON.parse(config);
 
   function findRangeMaxRacksNumber(racks) {
@@ -6,19 +6,47 @@ define(['text!config.json'], function (config) {
 
     var index;
     for (index in racks) {
-      if (Object.keys(racks[index]).length > number) {
-        number = Object.keys(racks[index]).length;
+      if (racks.hasOwnProperty(index)) {
+        if (Object.keys(racks[index]).length > number) {
+          number = Object.keys(racks[index]).length;
+        }
       }
     }
 
     return number;
   }
 
+
+  function findMapAltitude(racks) {
+    var altitude = 0;
+    var rackAltitude;
+
+    var rackIndex;
+    var nodeIndex;
+    var i;
+    for (rackIndex in racks) {
+      if (racks.hasOwnProperty(rackIndex)) {
+        for (nodeIndex in racks[rackIndex]) {
+          if (racks[rackIndex][nodeIndex]) {
+            for (i = 0; i < racks[rackIndex][nodeIndex].nodes.length; i++) {
+              rackAltitude = racks[rackIndex][nodeIndex].nodes[i].posy + racks[rackIndex][nodeIndex].nodes[i].height;
+              if (rackAltitude > altitude) {
+                altitude = rackAltitude;
+              } 
+            }
+          }
+        }
+      }
+    }
+
+    return altitude;
+  }
+
   function getMapWallX(max) {
     var walls = [];
 
     var i;
-    for (i = 0; i < max + 2 + (config.display.canvas3d.pathSize * 2); i++) {
+    for (i = 0; i < max + 2 + (config.PATHSIZE * 2); i++) {
       walls.push(1);
     }
 
@@ -31,11 +59,11 @@ define(['text!config.json'], function (config) {
     paths.push(1);
 
     var i;
-    for (i = 0; i < max + (config.display.canvas3d.pathSize * 2); i++) {
+    for (i = 0; i < max + (config.PATHSIZE * 2); i++) {
       paths.push(0);
     }
 
-    paths.push(1)
+    paths.push(1);
 
     return paths;
   }
@@ -45,12 +73,13 @@ define(['text!config.json'], function (config) {
 
     range.push(1);
     var i;
-    for (i = 0; i < config.display.canvas3d.pathSize; i++) {
+    for (i = 0; i < config.PATHSIZE; i++) {
       range.push(0);
     }
 
     i = 0;
-    for (var index in rack) {
+    var index;
+    for (index in rack) {
       if (rack.hasOwnProperty(index)) {
         range.push(rack[index].name)
       }
@@ -63,7 +92,7 @@ define(['text!config.json'], function (config) {
       }
     }
 
-    for (i = 0; i < config.display.canvas3d.pathSize; i++) {
+    for (i = 0; i < config.PATHSIZE; i++) {
       range.push(0);
     }
     range.push(1);
@@ -73,23 +102,35 @@ define(['text!config.json'], function (config) {
 
   return {
     racksToMap: function (racks) {
-      var map = [];
+      var map = {
+        data: [],
+        width: 0,
+        height: 0,
+        altitude: 0
+      };
+
       var rangeNumber = racks.length;
       var rangeMaxRacksNumber = findRangeMaxRacksNumber(racks);
 
       var index;
       var i;
-      map.push(getMapWallX(rangeMaxRacksNumber));
-      for (i = 0; i < config.display.canvas3d.pathSize; i++) {
-        map.push(getMapPathX(rangeMaxRacksNumber));
+      map.data = map.data.concat(getMapWallX(rangeMaxRacksNumber));
+      for (i = 0; i < config.PATHSIZE; i++) {
+        map.data = map.data.concat(getMapPathX(rangeMaxRacksNumber));
       }
+
       for (index in racks) {
-        map.push(getMapRangeX(racks[index], rangeMaxRacksNumber))
-        for (i = 0; i < config.display.canvas3d.pathSize; i++) {
-          map.push(getMapPathX(rangeMaxRacksNumber));
+        map.data = map.data.concat(getMapRangeX(racks[index], rangeMaxRacksNumber))
+        for (i = 0; i < config.PATHSIZE; i++) {
+          map.data = map.data.concat(getMapPathX(rangeMaxRacksNumber));
         }
       }
-      map.push(getMapWallX(rangeMaxRacksNumber));
+
+      map.data = map.data.concat(getMapWallX(rangeMaxRacksNumber));
+
+      map.width = rangeMaxRacksNumber + 2 + (config.PATHSIZE * 2);
+      map.height = map.data.length / map.width;
+      map.altitude = findMapAltitude(racks);
 
       return map;
     }
