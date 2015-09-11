004a244de64b9aacfeaf6f6507ff5ed55fe9f62d
Fabien Gavory <fabien@gavory.fr>
2015-09-11 16:14:57 +0200

    add dependencies for 3d view

--- js/core/index.js
+++ js/core/index.js
@@ -24,6 +24,8 @@ require.config({
     'nodes-utils': '../../js/utils/node',
     'page-utils': '../../js/utils/page',
     'ajax-utils': '../../js/utils/ajax',
+    'factor-utils': '../../js/utils/factor',
+    'draw-colors-utils': '../../js/utils/draw-colors',
     'draw-intersections-utils': '../../js/utils/draw-intersections',
     'draw-legend-utils': '../../js/utils/draw-legend',
     'draw-three-dimensional-utils': '../../js/utils/draw-three-dimensional',
@@ -35,7 +37,8 @@ require.config({
     partitions: '../../js/modules/partitions/partitions',
     qos: '../../js/modules/qos/qos',
     reservations: '../../js/modules/reservations/reservations',
-    'first-person-view': '../../js/modules/first-person-view/first-person-view',
+    '3d-view': '../../js/modules/3d-view/3d-view',
+    'three-first-person-controls': '../../js/modules/3d-view/camera/first-person-controls',
     gantt: '../../js/modules/gantt/gantt'
   },
   shim: {
@@ -63,7 +66,23 @@ require.config({
   }
 });
 
-require(['page-utils', 'text!config.json', 'token-utils', 'user-utils', 'login', 'navbar', 'jobs', 'racks', 'jobs-map', 'qos', 'partitions', 'reservations', 'first-person-view', 'gantt', 'ajax-utils'], function (Page, config, token, user, Login, Navbar, Jobs, Racks, JobsMap, QOS, Partitions, Reservations, FirstPersonView, Gantt) {
+require([
+  'page-utils',
+  'text!config.json',
+  'token-utils',
+  'user-utils',
+  'login',
+  'navbar',
+  'jobs',
+  'racks',
+  'jobs-map',
+  'qos',
+  'partitions',
+  'reservations',
+  '3d-view',
+  'gantt',
+  'ajax-utils'
+], function (Page, config, token, user, Login, Navbar, Jobs, Racks, JobsMap, QOS, Partitions, Reservations, d3View, Gantt) {
   var navbar = new Navbar();
   var page = new Page();
 
@@ -104,8 +123,8 @@ require(['page-utils', 'text!config.json', 'token-utils', 'user-utils', 'login',
     case 'reservations':
       $.extend(page,  new Page('reservations'), new Reservations());
       break;
-    case 'firstpersonview':
-      $.extend(page, new Page('3dview'), new FirstPersonView());
+    case '3dview':
+      $.extend(page, new Page('3dview'), new d3View());
       break;
     case 'gantt':
       $.extend(page,  new Page('gantt'), new Gantt());
