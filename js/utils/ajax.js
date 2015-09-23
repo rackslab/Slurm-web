dfe3ce09f682d9bf86d0841438605b141f847fa2
Lorel <lorel@wellatribe.com>
2015-09-23 12:30:29 +0200

    add flash message for ajax errors in dashboard

--- js/utils/ajax.js
+++ js/utils/ajax.js
@@ -4,6 +4,8 @@ define([
   $(document).ajaxError(function (event, jqueryXHR, error, errorThrown) {
     if (!jqueryXHR.status) {
       console.log(JSON.stringify(event), JSON.stringify(jqueryXHR), JSON.stringify(error), JSON.stringify(errorThrown));
+      $('#flash .alert').text("Error : " + JSON.stringify(error));
+      $('#flash').show();
     }
     if ((jqueryXHR.status === 403) && (error.url !== '/slurm-restapi/login')) {
       $(document).trigger('logout');
