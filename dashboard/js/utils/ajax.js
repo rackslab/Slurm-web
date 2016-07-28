7e8ae249310279f8a55205c8adc59f5bc771fc19
Rémi Palancher <remi-externe.palancher@edf.fr>
2016-07-28 14:53:43 +0200

    Show exact error in ajax error handler
    The ajaxError jQuery handler considered all error as being
    authentication error and reported them as such in the error #flash.
    This is obviously not the case as many different kinds of errors
    are possible. The new logic tries to report the exact error when
    possible (ie. when not unexpected error with CORS), except on
    /authentication route.
    
    This commit also adds some hopefully useful comments to explain the
    managed cases here.

--- dashboard/js/utils/ajax.js
+++ dashboard/js/utils/ajax.js
@@ -23,12 +23,33 @@ define([
 ], function($) {
   $(document).ajaxError(function(event, jqueryXHR, error, errorThrown) {
     console.log(JSON.stringify(event), JSON.stringify(jqueryXHR), JSON.stringify(error), JSON.stringify(errorThrown));  // eslint-disable-line no-console
-    if (!jqueryXHR.status && !(error.url.indexOf('/authentication') > -1)) {
-      $('#flash .alert').append($('<p>').text('Error : unable to perform authentication'));
-      $('#flash').show();
-    }
+
+    // In case of 403 error (forbidden), logout from cluster(s) except for
+    // /login route since this error in handled with login form logic in
+    // dashboard/js/core/login/login.js
     if (jqueryXHR.status === 403 && !(error.url.indexOf('/login') > -1)) {
       $(document).trigger('logout');
+    }
+
+    // Ignore error for /authentication here because this route is used to
+    // probe cluster availability and API authentication configuration on
+    // dashboard load and the errors on this route are directly handled in
+    // dashboard/js/core/clusters/clusters.js
+    if (!(error.url.indexOf('/authentication') > -1)) {
+
+      // Show error in #flash.
+      // When the REST API fails in unexpected way (typically with 500 internal
+      // error) and CORS is enabled, the REST API is unable to set CORS
+      // Access-Control headers properly in its response. This results in the
+      // browser blocking the response and not propagating the response HTTP
+      // status to jquery XHR. It is the reason why we check for unknown jXHR
+      // status here.
+      if (!jqueryXHR.status) {
+        $('#flash .alert').append($('<p>').text('Unknown error on request ' + error.type + ' ' + error.url));
+      } else {
+        $('#flash .alert').append($('<p>').text('Error ' + jqueryXHR.status + ' on request ' + error.type + ' ' + error.url));
+      }
+      $('#flash').show();
     }
   });
 });
