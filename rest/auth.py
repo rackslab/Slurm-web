rackslab/Slurm-web

commit 18d0866c765b7e591c654d8fd4f2018ba1b64a49

Correctly check ldaps SSL server certificate
When the LDAP server URI starts with ldaps, the server SSL certificate
is now fully checked. Eventually against the CA certificate given in
the configuration file if needed.

Fixes #133

rest/auth.py
@@ -88,12 +88,24 @@ try:
 except NoSectionError:
     acl = []
 
-ldap.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_NEVER)
-
 
 def get_ldap_connection():
-    conn = ldap.initialize(settings.get('ldap', 'uri'))
-    return conn
+
+    uri = settings.get('ldap', 'uri')
+    conn = ldap.initialize(uri)
+
+    # LDAP/SSL setup
+    if uri.startswith('ldaps'):
+
+        conn.protocol_version = ldap.VERSION3
+        # Force libldap to create a new SSL context
+        conn.set_option(ldap.OPT_X_TLS_NEWCTX, ldap.OPT_X_TLS_DEMAND)
+        # Force cert validation
+        conn.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_DEMAND)
+        if settings.has_option('ldap', 'cacert'):
+            cacert = settings.get('ldap', 'cacert')
+            # Set path name of file containing all trusted CA certificates
+            conn.set_option(ldap.OPT_X_TLS_CACERTFILE, cacert)
 
 
 def filter_dict(to_filter={}, filtered_keys=[]):

conf/restapi.conf
@@ -71,6 +71,9 @@ restricted_fields_for_admin =
 uri = ldap://admin:389
 base_people = ou=people,dc=cluster,dc=local
 base_group = ou=group,dc=cluster,dc=local
+# Optional path to CA certificate. Required only if ldaps protocol is used and
+# CA certification not available in system certificates chain.
+# cacert = /etc/ssl/certs/cacert.pem
 expiration = 1296000
 
 [cache]

commit bf9e2dc4f4a58160687dea379bd8d744f5a9706a

Fix missing return in get_ldap_connection()
This bug was introduced with 18d0866.

rest/auth.py
@@ -107,6 +107,7 @@ def get_ldap_connection():
             # Set path name of file containing all trusted CA certificates
             conn.set_option(ldap.OPT_X_TLS_CACERTFILE, cacert)
 
+    return conn
 
 def filter_dict(to_filter={}, filtered_keys=[]):
     if isinstance(to_filter, dict):