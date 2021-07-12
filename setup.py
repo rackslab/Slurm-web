9664d8883203ca6da368c34f2880298940da049b
Rémi Palancher <remi-externe.palancher@edf.fr>
2021-07-12 16:18:00 +0200

    Use setup.py instead of setup.cfg
    Old versions of setuptools we want to support require the presence
    of setup.py. Then just use setup.py instead of static setup.cfg.

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='slurm-web',
+    version='2.3.1',
+    author='EDF CCN-HPC',
+    author_email='dsp-cspit-ccn-hpc@edf.fr',
+    description='Web solution for Slurm HPC job schedulers',
+    long_description=long_description,
+    url='https://github.com/edf-hpc/slurm-web',
+    package_dir={'': 'src'},
+    packages=setuptools.find_packages(where='src'),
+    package_data={'': '*.wsgi'},
+    install_requires=['Flask',
+                      'ClusterShell',
+                      'pyslurm',
+                      'python-ldap',
+                      'redis'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: POSIX :: Linux',
+    ],
+    python_requires='>=3.5',
+)
