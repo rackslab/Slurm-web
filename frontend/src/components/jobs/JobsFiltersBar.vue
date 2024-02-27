c37f17e856823e8e36a101cdd38d2c587c3000d2
Rémi Palancher <remi@rackslab.io>
2024-02-27 16:23:30 +0100

    refactor: move jobs filters bar in component

---
+++ frontend/src/components/jobs/JobsFiltersBar.vue
@@ -0,0 +1,122 @@
+<script setup lang="ts">
+import { useRuntimeStore } from '@/stores/runtime'
+import {
+  FunnelIcon,
+  BoltIcon,
+  RectangleGroupIcon,
+  UserIcon,
+  UsersIcon,
+  SwatchIcon
+} from '@heroicons/vue/20/solid'
+
+const runtimeStore = useRuntimeStore()
+</script>
+
+<template>
+  <!-- Active filters -->
+  <div v-show="!runtimeStore.jobs.emptyFilters()" class="bg-gray-100">
+    <div class="mx-auto px-4 py-3 sm:flex sm:items-center sm:px-6 lg:px-8">
+      <h3 class="text-sm font-medium text-gray-500">
+        <FunnelIcon class="mr-1 h-4 w-4" />
+        <span class="sr-only">Filters active</span>
+      </h3>
+
+      <div aria-hidden="true" class="hidden h-5 w-px bg-gray-300 sm:ml-4 sm:block" />
+
+      <div class="mt-2 sm:ml-4 sm:mt-0">
+        <div class="-m-1 flex flex-wrap items-center">
+          <span
+            v-for="activeStateFilter in runtimeStore.jobs.filters.states"
+            :key="activeStateFilter"
+            class="m-1 inline-flex items-center rounded-full border border-gray-200 bg-gray-600 py-1.5 pl-3 pr-2 text-sm font-medium text-white"
+          >
+            <BoltIcon class="mr-1 h-4 w-4" />
+            <span>{{ activeStateFilter }}</span>
+            <button
+              type="button"
+              class="ml-1 inline-flex h-4 w-4 flex-shrink-0 rounded-full p-1 text-gray-400 hover:bg-gray-700 hover:text-gray-500"
+              @click="runtimeStore.jobs.removeStateFilter(activeStateFilter)"
+            >
+              <span class="sr-only">Remove filter for state:{{ activeStateFilter }}</span>
+              <svg class="h-2 w-2" stroke="currentColor" fill="none" viewBox="0 0 8 8">
+                <path stroke-linecap="round" stroke-width="1.5" d="M1 1l6 6m0-6L1 7" />
+              </svg>
+            </button>
+          </span>
+          <span
+            v-for="activeUserFilter in runtimeStore.jobs.filters.users"
+            :key="activeUserFilter"
+            class="m-1 inline-flex items-center rounded-full border border-gray-200 bg-emerald-500 py-1.5 pl-3 pr-2 text-sm font-medium text-white"
+          >
+            <UserIcon class="mr-1 h-4 w-4" />
+            <span>{{ activeUserFilter }}</span>
+            <button
+              type="button"
+              class="ml-1 inline-flex h-4 w-4 flex-shrink-0 rounded-full p-1 text-emerald-600 hover:bg-emerald-600 hover:text-emerald-700"
+              @click="runtimeStore.jobs.removeUserFilter(activeUserFilter)"
+            >
+              <span class="sr-only">Remove filter for user:{{ activeUserFilter }}</span>
+              <svg class="h-2 w-2" stroke="currentColor" fill="none" viewBox="0 0 8 8">
+                <path stroke-linecap="round" stroke-width="1.5" d="M1 1l6 6m0-6L1 7" />
+              </svg>
+            </button>
+          </span>
+          <span
+            v-for="activeAccountFilter in runtimeStore.jobs.filters.accounts"
+            :key="activeAccountFilter"
+            class="m-1 inline-flex items-center rounded-full border border-gray-200 bg-yellow-500 py-1.5 pl-3 pr-2 text-sm font-medium text-white"
+          >
+            <UsersIcon class="mr-1 h-4 w-4" />
+            <span>{{ activeAccountFilter }}</span>
+            <button
+              type="button"
+              class="ml-1 inline-flex h-4 w-4 flex-shrink-0 rounded-full p-1 text-yellow-600 hover:bg-yellow-600 hover:text-yellow-700"
+              @click="runtimeStore.jobs.removeAccountFilter(activeAccountFilter)"
+            >
+              <span class="sr-only">Remove filter for account:{{ activeAccountFilter }}</span>
+              <svg class="h-2 w-2" stroke="currentColor" fill="none" viewBox="0 0 8 8">
+                <path stroke-linecap="round" stroke-width="1.5" d="M1 1l6 6m0-6L1 7" />
+              </svg>
+            </button>
+          </span>
+          <span
+            v-for="activeQosFilter in runtimeStore.jobs.filters.qos"
+            :key="activeQosFilter"
+            class="m-1 inline-flex items-center rounded-full border border-gray-200 bg-purple-500 py-1.5 pl-3 pr-2 text-sm font-medium text-white"
+          >
+            <SwatchIcon class="mr-1 h-4 w-4" />
+            <span>{{ activeQosFilter }}</span>
+            <button
+              type="button"
+              class="ml-1 inline-flex h-4 w-4 flex-shrink-0 rounded-full p-1 text-purple-600 hover:bg-purple-600 hover:text-purple-700"
+              @click="runtimeStore.jobs.removeQosFilter(activeQosFilter)"
+            >
+              <span class="sr-only">Remove filter for qos:{{ activeQosFilter }}</span>
+              <svg class="h-2 w-2" stroke="currentColor" fill="none" viewBox="0 0 8 8">
+                <path stroke-linecap="round" stroke-width="1.5" d="M1 1l6 6m0-6L1 7" />
+              </svg>
+            </button>
+          </span>
+          <span
+            v-for="activePartitionFilter in runtimeStore.jobs.filters.partitions"
+            :key="activePartitionFilter"
+            class="m-1 inline-flex items-center rounded-full border border-gray-200 bg-amber-700 py-1.5 pl-3 pr-2 text-sm font-medium text-white"
+          >
+            <RectangleGroupIcon class="mr-1 h-4 w-4" />
+            <span>{{ activePartitionFilter }}</span>
+            <button
+              type="button"
+              class="ml-1 inline-flex h-4 w-4 flex-shrink-0 rounded-full p-1 text-amber-800 hover:bg-amber-800 hover:text-amber-900"
+              @click="runtimeStore.jobs.removePartitionFilter(activePartitionFilter)"
+            >
+              <span class="sr-only">Remove filter for partition:{{ activePartitionFilter }}</span>
+              <svg class="h-2 w-2" stroke="currentColor" fill="none" viewBox="0 0 8 8">
+                <path stroke-linecap="round" stroke-width="1.5" d="M1 1l6 6m0-6L1 7" />
+              </svg>
+            </button>
+          </span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- frontend/src/views/JobsView.vue
+++ frontend/src/views/JobsView.vue
@@ -10,17 +10,10 @@ import JobsSorter from '@/components/jobs/JobsSorter.vue'
 import JobStatusLabel from '@/components/jobs/JobStatusLabel.vue'
 import ClusterMainLayout from '@/components/ClusterMainLayout.vue'
 import JobsFiltersPanel from '@/components/jobs/JobsFiltersPanel.vue'
+import JobsFiltersBar from '@/components/jobs/JobsFiltersBar.vue'
 import { PlusSmallIcon } from '@heroicons/vue/24/outline'
-import {
-  FunnelIcon,
-  BoltIcon,
-  UserIcon,
-  UsersIcon,
-  ChevronLeftIcon,
-  ChevronRightIcon,
-  SwatchIcon,
-  RectangleGroupIcon
-} from '@heroicons/vue/20/solid'
+
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/vue/20/solid'
 
 const props = defineProps({
   cluster: {
@@ -281,7 +274,6 @@ onMounted(() => {
           <div class="h-14 w-14 rounded-2xl bg-slate-200"></div>
         </div>
       </div>
-
       <section aria-labelledby="filter-heading" class="-mx-4 -my-2 sm:-mx-6 lg:-mx-8">
         <h2 id="filter-heading" class="sr-only">Filters</h2>
 
@@ -299,115 +291,7 @@ onMounted(() => {
             </button>
           </div>
         </div>
-
-        <!-- Active filters -->
-        <div v-show="!runtimeStore.jobs.emptyFilters()" class="bg-gray-100">
-          <div class="mx-auto px-4 py-3 sm:flex sm:items-center sm:px-6 lg:px-8">
-            <h3 class="text-sm font-medium text-gray-500">
-              <FunnelIcon class="mr-1 h-4 w-4" />
-              <span class="sr-only">Filters active</span>
-            </h3>
-
-            <div aria-hidden="true" class="hidden h-5 w-px bg-gray-300 sm:ml-4 sm:block" />
-
-            <div class="mt-2 sm:ml-4 sm:mt-0">
-              <div class="-m-1 flex flex-wrap items-center">
-                <span
-                  v-for="activeStateFilter in runtimeStore.jobs.filters.states"
-                  :key="activeStateFilter"
-                  class="m-1 inline-flex items-center rounded-full border border-gray-200 bg-gray-600 py-1.5 pl-3 pr-2 text-sm font-medium text-white"
-                >
-                  <BoltIcon class="mr-1 h-4 w-4" />
-                  <span>{{ activeStateFilter }}</span>
-                  <button
-                    type="button"
-                    class="ml-1 inline-flex h-4 w-4 flex-shrink-0 rounded-full p-1 text-gray-400 hover:bg-gray-700 hover:text-gray-500"
-                    @click="runtimeStore.jobs.removeStateFilter(activeStateFilter)"
-                  >
-                    <span class="sr-only">Remove filter for state:{{ activeStateFilter }}</span>
-                    <svg class="h-2 w-2" stroke="currentColor" fill="none" viewBox="0 0 8 8">
-                      <path stroke-linecap="round" stroke-width="1.5" d="M1 1l6 6m0-6L1 7" />
-                    </svg>
-                  </button>
-                </span>
-                <span
-                  v-for="activeUserFilter in runtimeStore.jobs.filters.users"
-                  :key="activeUserFilter"
-                  class="m-1 inline-flex items-center rounded-full border border-gray-200 bg-emerald-500 py-1.5 pl-3 pr-2 text-sm font-medium text-white"
-                >
-                  <UserIcon class="mr-1 h-4 w-4" />
-                  <span>{{ activeUserFilter }}</span>
-                  <button
-                    type="button"
-                    class="ml-1 inline-flex h-4 w-4 flex-shrink-0 rounded-full p-1 text-emerald-600 hover:bg-emerald-600 hover:text-emerald-700"
-                    @click="runtimeStore.jobs.removeUserFilter(activeUserFilter)"
-                  >
-                    <span class="sr-only">Remove filter for user:{{ activeUserFilter }}</span>
-                    <svg class="h-2 w-2" stroke="currentColor" fill="none" viewBox="0 0 8 8">
-                      <path stroke-linecap="round" stroke-width="1.5" d="M1 1l6 6m0-6L1 7" />
-                    </svg>
-                  </button>
-                </span>
-                <span
-                  v-for="activeAccountFilter in runtimeStore.jobs.filters.accounts"
-                  :key="activeAccountFilter"
-                  class="m-1 inline-flex items-center rounded-full border border-gray-200 bg-yellow-500 py-1.5 pl-3 pr-2 text-sm font-medium text-white"
-                >
-                  <UsersIcon class="mr-1 h-4 w-4" />
-                  <span>{{ activeAccountFilter }}</span>
-                  <button
-                    type="button"
-                    class="ml-1 inline-flex h-4 w-4 flex-shrink-0 rounded-full p-1 text-yellow-600 hover:bg-yellow-600 hover:text-yellow-700"
-                    @click="runtimeStore.jobs.removeAccountFilter(activeAccountFilter)"
-                  >
-                    <span class="sr-only">Remove filter for account:{{ activeAccountFilter }}</span>
-                    <svg class="h-2 w-2" stroke="currentColor" fill="none" viewBox="0 0 8 8">
-                      <path stroke-linecap="round" stroke-width="1.5" d="M1 1l6 6m0-6L1 7" />
-                    </svg>
-                  </button>
-                </span>
-                <span
-                  v-for="activeQosFilter in runtimeStore.jobs.filters.qos"
-                  :key="activeQosFilter"
-                  class="m-1 inline-flex items-center rounded-full border border-gray-200 bg-purple-500 py-1.5 pl-3 pr-2 text-sm font-medium text-white"
-                >
-                  <SwatchIcon class="mr-1 h-4 w-4" />
-                  <span>{{ activeQosFilter }}</span>
-                  <button
-                    type="button"
-                    class="ml-1 inline-flex h-4 w-4 flex-shrink-0 rounded-full p-1 text-purple-600 hover:bg-purple-600 hover:text-purple-700"
-                    @click="runtimeStore.jobs.removeQosFilter(activeQosFilter)"
-                  >
-                    <span class="sr-only">Remove filter for qos:{{ activeQosFilter }}</span>
-                    <svg class="h-2 w-2" stroke="currentColor" fill="none" viewBox="0 0 8 8">
-                      <path stroke-linecap="round" stroke-width="1.5" d="M1 1l6 6m0-6L1 7" />
-                    </svg>
-                  </button>
-                </span>
-                <span
-                  v-for="activePartitionFilter in runtimeStore.jobs.filters.partitions"
-                  :key="activePartitionFilter"
-                  class="m-1 inline-flex items-center rounded-full border border-gray-200 bg-amber-700 py-1.5 pl-3 pr-2 text-sm font-medium text-white"
-                >
-                  <RectangleGroupIcon class="mr-1 h-4 w-4" />
-                  <span>{{ activePartitionFilter }}</span>
-                  <button
-                    type="button"
-                    class="ml-1 inline-flex h-4 w-4 flex-shrink-0 rounded-full p-1 text-amber-800 hover:bg-amber-800 hover:text-amber-900"
-                    @click="runtimeStore.jobs.removePartitionFilter(activePartitionFilter)"
-                  >
-                    <span class="sr-only"
-                      >Remove filter for partition:{{ activePartitionFilter }}</span
-                    >
-                    <svg class="h-2 w-2" stroke="currentColor" fill="none" viewBox="0 0 8 8">
-                      <path stroke-linecap="round" stroke-width="1.5" d="M1 1l6 6m0-6L1 7" />
-                    </svg>
-                  </button>
-                </span>
-              </div>
-            </div>
-          </div>
-        </div>
+        <JobsFiltersBar />
       </section>
 
       <div class="mt-8 flow-root">
