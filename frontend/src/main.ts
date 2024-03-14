124604ea877bbd5afbada141efe76f8291f3d682
Rémi Palancher <remi@rackslab.io>
2024-03-14 16:17:06 +0100

    fix(front): await runtime conf before create app
    Ensure the runtime configuration is fully loaded before creating the
    app.

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -15,9 +15,11 @@ import router from './router'
 import { initRuntimeConfiguration, runtimeConfiguration } from '@/plugins/runtimeConfiguration'
 import { httpPlugin } from '@/plugins/http'
 
+const runtimeConfigurationOptions = await initRuntimeConfiguration()
+
 const app = createApp(App)
 app.use(createPinia())
 app.use(router)
-app.use(runtimeConfiguration, await initRuntimeConfiguration())
+app.use(runtimeConfiguration, runtimeConfigurationOptions)
 app.use(httpPlugin)
 app.mount('#app')
