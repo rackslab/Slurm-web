b1a8600c561fe7f51532a972e262a9625e5316d5
Rémi Palancher <remi@rackslab.io>
2024-03-14 16:18:41 +0100

    feat: introduce slurm-web-gen-jwt-key
    Script to generate JWT signing key for both the gateway and agent
    components.

---
+++ slurmweb/apps/genjwt.py
@@ -0,0 +1,59 @@
+# Copyright (c) 2024 Rackslab
+#
+# This file is part of Slurm-web.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import os
+import sys
+import pwd
+import subprocess
+import shlex
+import logging
+
+from . import SlurmwebGenericApp
+
+from rfl.authentication.jwt import jwt_gen_key
+from rfl.authentication.errors import JWTPrivateKeyGeneratorError
+logger = logging.getLogger(__name__)
+
+
+class SlurmwebAppGenJWT(SlurmwebGenericApp):
+
+    NAME = "slurm-web-gen-jwt-key"
+
+    def run(self):
+        logger.info("Running %s", self.NAME)
+
+        if os.geteuid():
+            logger.critical("This script must run as root")
+            sys.exit(1)
+        if self.settings.jwt.key.exists():
+            logger.warning("JWT key %s already exist", self.settings.jwt.key)
+        else:
+            try:
+                jwt_gen_key(self.settings.jwt.key)
+            except JWTPrivateKeyGeneratorError as err:
+                logger.critical("Error while generating JWT key: %s", err)
+                sys.exit(1)
+
+        for user in ["slurm-web", "slurm"]:
+            self.set_user_permission(user)
+
+    def set_user_permission(self, user):
+        try:
+            pwd.getpwnam(user)
+        except KeyError:
+            logger.warning(
+                "User %s not found, unable to set permission on JWT key for this user",
+                user,
+            )
+        else:
+            try:
+                logger.info("Setting read permission on key for %s user", user)
+                cmd = ["setfacl", "-m", f"u:{user}:r", self.settings.jwt.key]
+                subprocess.run(cmd)
+            except subprocess.CalledProcessError as err:
+                logger.error(
+                    "Error while running command: %s: %s", shlex.join(cmd), err
+                )
---
+++ slurmweb/exec/genjwt.py
@@ -0,0 +1,66 @@
+# Copyright (c) 2023 Rackslab
+#
+# This file is part of Slurm-web.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import argparse
+from pathlib import Path
+
+from ..version import get_version
+from . import SlurmwebAppArgs
+from ..apps import SlurmwebConfSeed
+from ..apps.genjwt import SlurmwebAppGenJWT
+from ..apps.gateway import SlurmwebAppGateway
+
+
+class SlurmwebExecGenJWT:
+    @staticmethod
+    def run():
+        parser = argparse.ArgumentParser(description=SlurmwebAppGenJWT.NAME)
+        parser.add_argument(
+            "-v",
+            "--version",
+            dest="version",
+            action="version",
+            version="%(prog)s " + get_version(),
+        )
+        parser.add_argument(
+            "--debug",
+            dest="debug",
+            action="store_true",
+            help="Enable debug mode",
+        )
+        parser.add_argument(
+            "--log-flags",
+            help="Log flags (default: %(default)s)",
+            default="ALL",
+            nargs="*",
+            choices=["slurmweb", "rfl", "racksdb", "werkzeug", "urllib3", "ALL"],
+        )
+        parser.add_argument(
+            "--debug-flags",
+            help="Debug flags (default: %(default)s)",
+            default="slurmweb",
+            nargs="*",
+            choices=["slurmweb", "rfl", "racksdb", "werkzeug", "urllib3", "ALL"],
+        )
+        parser.add_argument(
+            "--conf-defs",
+            help=(
+                "Path to configuration settings definition file (default: %(default)s)"
+            ),
+            default=SlurmwebAppGateway.SETTINGS_DEFINITION,
+            type=Path,
+        )
+        parser.add_argument(
+            "--conf",
+            help="Path to configuration file (default: %(default)s)",
+            default=SlurmwebAppGateway.SITE_CONFIGURATION,
+            type=Path,
+        )
+
+        application = SlurmwebAppGenJWT(
+            SlurmwebConfSeed.from_args(parser.parse_args(namespace=SlurmwebAppArgs))
+        )
+        application.run()
