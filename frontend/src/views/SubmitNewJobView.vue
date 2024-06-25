8d212895bc8c98479df3f7ca938c2a11e74f9056
Florian Lespérant <florian.lesperant@rackslab.io>
2024-06-25 11:18:48 +0200

    feat(front): Add `Back to jobs` return button

--- frontend/src/views/SubmitNewJobView.vue
+++ frontend/src/views/SubmitNewJobView.vue
@@ -8,6 +8,7 @@
 
 <script setup lang="ts">
 import ClusterMainLayout from '@/components/ClusterMainLayout.vue'
+import { ChevronLeftIcon } from '@heroicons/vue/20/solid'
 
 const props = defineProps({
   cluster: {
@@ -18,5 +19,15 @@ const props = defineProps({
 </script>
 
 <template>
-  <ClusterMainLayout :cluster="props.cluster" title="Submit new job"> </ClusterMainLayout>
+  <ClusterMainLayout :cluster="props.cluster" title="Submit new job">
+    <router-link :to="{ name: 'jobs' }"
+      ><button
+        type="button"
+        class="mb-16 ml-5 mt-8 inline-flex items-center gap-x-2 rounded-md bg-slurmweb px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-slurmweb-dark focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-slurmweb-dark"
+      >
+        <ChevronLeftIcon class="-ml-0.5 h-5 w-5" aria-hidden="true" />
+        Back to jobs
+      </button></router-link
+    >
+  </ClusterMainLayout>
 </template>
