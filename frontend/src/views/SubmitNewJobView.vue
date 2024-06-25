d76e4e57ae5dc32cfde95560f4fbc6230b7504f7
Florian Lespérant <florian.lesperant@rackslab.io>
2024-06-25 11:10:43 +0200

    feat(front): Create SubmitNewJobView and its route

---
+++ frontend/src/views/SubmitNewJobView.vue
@@ -0,0 +1,22 @@
+<!--
+  Copyright (c) 2023-2024 Rackslab
+
+  This file is part of Slurm-web.
+
+  SPDX-License-Identifier: GPL-3.0-or-later
+-->
+
+<script setup lang="ts">
+import ClusterMainLayout from '@/components/ClusterMainLayout.vue'
+
+const props = defineProps({
+  cluster: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <ClusterMainLayout :cluster="props.cluster" title="Submit new job"> </ClusterMainLayout>
+</template>
