bfb61bf80bc0cdc0b14dc073f152807dc3eb12d0
Florian Lespérant <florian.lesperant@rackslab.io>
2024-06-26 11:00:22 +0200

    feat(front): Get inputs data from the gateway
    - Create Input interface, add 'inputs' to the GatewayClusterAPIKeys.
    - Create 'inputs' function  to get an array of inputs.

--- frontend/src/composables/GatewayAPI.ts
+++ frontend/src/composables/GatewayAPI.ts
@@ -260,6 +260,18 @@ export interface Template {
   description: string
 }
 
+export interface Input {
+  id: number
+  name: string
+  description: string
+  default: string
+  minVal: number
+  maxVal: number
+  regex: string
+  template: number
+  type: number
+}
+
 export function renderClusterOptionalNumber(optionalNumber: ClusterOptionalNumber): string {
   if (!optionalNumber.set) {
     return '-'
@@ -357,7 +369,8 @@ const GatewayClusterAPIKeys = [
   'qos',
   'reservations',
   'accounts',
-  'templates'
+  'templates',
+  'inputs'
 ] as const
 export type GatewayClusterAPIKey = (typeof GatewayClusterAPIKeys)[number]
 const GatewayClusterWithNumberAPIKeys = ['job'] as const
@@ -531,6 +544,10 @@ export function useGatewayAPI() {
     return await get<Template[]>(`/agents/${cluster}/templates`)
   }
 
+  async function inputs(cluster: string): Promise<Array<Input>> {
+    return await get<Input[]>(`/agents/${cluster}/inputs`)
+  }
+
   async function infrastructureImagePng(
     cluster: string,
     width: number,
@@ -604,6 +621,7 @@ export function useGatewayAPI() {
     reservations,
     accounts,
     templates,
+    inputs,
     infrastructureImagePng,
     abort,
     isValidGatewayGenericAPIKey,
