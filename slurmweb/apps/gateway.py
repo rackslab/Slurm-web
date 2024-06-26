3a5aa3c5de0d6814172e3586b42c1ff784d9af60
Florian Lespérant <florian.lesperant@rackslab.io>
2024-06-26 10:58:33 +0200

    feat(gateway): Get inputs data from the agent

--- slurmweb/apps/gateway.py
+++ slurmweb/apps/gateway.py
@@ -54,6 +54,7 @@ class SlurmwebAppGateway(SlurmwebWebApp, RFLTokenizedWebApp):
         SlurmwebAppRoute("/api/agents/<cluster>/reservations", views.reservations),
         SlurmwebAppRoute("/api/agents/<cluster>/accounts", views.accounts),
         SlurmwebAppRoute("/api/agents/<cluster>/templates", views.templates),
+        SlurmwebAppRoute("/api/agents/<cluster>/inputs", views.inputs),
         SlurmwebAppRoute(
             "/api/agents/<cluster>/racksdb/<path:query>",
             views.racksdb,
