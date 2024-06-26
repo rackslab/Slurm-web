ab64c55300c5b305a695cf95c6a94ba105fb48c9
Florian Lespérant <florian.lesperant@rackslab.io>
2024-06-26 11:04:44 +0200

    feat(front): Add template inputs to the form

--- frontend/src/views/JobConfigurationView.vue
+++ frontend/src/views/JobConfigurationView.vue
@@ -12,9 +12,11 @@ import { ChevronLeftIcon } from '@heroicons/vue/20/solid'
 import { onMounted, ref } from 'vue'
 import { useGatewayAPI } from '@/composables/GatewayAPI'
 import type { Ref } from 'vue'
+import type { Input } from '@/composables/GatewayAPI'
 
 const gateway = useGatewayAPI()
 const selectedTemplate: Ref<Record<string, string>> = ref({})
+const inputs: Ref<Array<Input>> = ref([])
 
 const props = defineProps({
   cluster: {
@@ -29,6 +31,7 @@ const props = defineProps({
 
 onMounted(async () => {
   let getTemplates = await gateway.templates(props.cluster)
+  let getInputs = await gateway.inputs(props.cluster)
 
   getTemplates.forEach((template) => {
     if (template.id == Number(props.idTemplate)) {
@@ -36,6 +39,7 @@ onMounted(async () => {
       selectedTemplate.value['description'] = template.description
     }
   })
+  inputs.value = getInputs.filter((input) => input.template == Number(props.idTemplate))
 })
 </script>
 
@@ -57,6 +61,33 @@ onMounted(async () => {
         <p class="mt-4 max-w-xl text-sm font-light text-gray-600">
           {{ selectedTemplate.description }}
         </p>
+
+        <div v-for="input in inputs" :key="input.id" class="flex items-center pt-10">
+          <div class="w-52 flex-col text-left">
+            <p class="text-sm font-medium text-gray-900">{{ input.name }}</p>
+            <p class="mt-1 text-sm text-gray-500">{{ input.description }}</p>
+          </div>
+          <input
+            type="text"
+            class="border-1 ml-5 w-96 rounded-md border-solid bg-white py-1.5 pl-3 pr-10 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:border-slurmweb focus:outline-none sm:text-sm sm:leading-6"
+            required
+          />
+          <p class="mt-1 pl-5 text-sm text-gray-500">{{ input.default }}</p>
+        </div>
+        <div class="flex justify-end">
+          <button
+            type="button"
+            class="text-blck mb-16 mt-8 inline-flex w-20 justify-center gap-x-2 rounded-md bg-gray-200 px-3.5 py-2.5 text-sm font-semibold shadow-sm hover:bg-gray-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-slurmweb-dark"
+          >
+            Cancel
+          </button>
+          <button
+            type="button"
+            class="mb-16 ml-5 mt-8 inline-flex w-20 justify-center gap-x-2 rounded-md bg-slurmweb px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-slurmweb-dark focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-slurmweb-dark"
+          >
+            Submit
+          </button>
+        </div>
       </div>
     </div>
   </ClusterMainLayout>
