d22cd8af8c5d87e6eb9c0a88c9ff91a1cab01825
Florian Lespérant <florian.lesperant@rackslab.io>
2024-07-24 16:19:28 +0200

    fix(front): types in createTemplate store

--- frontend/src/stores/forms/createTemplate.ts
+++ frontend/src/stores/forms/createTemplate.ts
@@ -4,19 +4,19 @@ export const useTemplateStore = defineStore('templateStore', {
   state: () => ({
     name: '',
     description: '',
-    userAccounts: Array<string>,
-    userLogins: Array<string>,
-    developerAccounts: Array<string>,
-    developerLogins: Array<string>
+    userAccounts: [] as string[],
+    userLogins: [] as string[],
+    developerAccounts: [] as string[],
+    developerLogins: [] as string[]
   }),
   actions: {
     resetTemplate() {
       ;(this.name = ''),
         (this.description = ''),
-        (this.userAccounts = Array<string>),
-        (this.userLogins = Array<string>),
-        (this.developerAccounts = Array<string>),
-        (this.developerLogins = Array<string>)
+        (this.userAccounts = []),
+        (this.userLogins = []),
+        (this.developerAccounts = []),
+        (this.developerLogins = [])
     }
   }
 })
