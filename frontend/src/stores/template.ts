601b76e0fe7d7bf2ee88467e8a1e399bbaabd302
Florian Lespérant <florian.lesperant@rackslab.io>
2024-07-25 14:47:19 +0200

    fix(front): Merge create template and input stores
    Create a new store `template.ts`, add all the code of:
    - createTemplate
    - createInput
    
    Delete `createTemplate` and `createInput` the folder `forms`.
    Also use composition api syntax instead of option api.

---
+++ frontend/src/stores/template.ts
@@ -0,0 +1,85 @@
+/*
+ * Copyright (c) 2023-2024 Rackslab
+ *
+ * This file is part of Slurm-web.
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+import type { Ref } from 'vue'
+
+interface Input {
+  name: string
+  description: string
+  default: string
+  minVal: string
+  maxVal: string
+  regex: string
+  type: string
+}
+
+export const useTemplateStore = defineStore('template', () => {
+  const name: Ref<string> = ref('')
+  const description: Ref<string> = ref('')
+  const userAccounts: Ref<Array<string>> = ref([])
+  const userLogins: Ref<Array<string>> = ref([])
+  const developerAccounts: Ref<Array<string>> = ref([])
+  const developerLogins: Ref<Array<string>> = ref([])
+  const inputs: Ref<Array<Input>> = ref([])
+  const batchScript: Ref<string> = ref('')
+  const stagingInput: Ref<Input> = ref({
+    name: '',
+    description: '',
+    default: '',
+    minVal: '',
+    maxVal: '',
+    regex: '',
+    type: ''
+  })
+
+  function resetTemplate() {
+    name.value = ''
+    description.value = ''
+    userAccounts.value = []
+    userLogins.value = []
+    developerAccounts.value = []
+    developerLogins.value = []
+    inputs.value = []
+  }
+
+  function resetInput() {
+    stagingInput.value = {
+      name: '',
+      description: '',
+      default: '',
+      minVal: '',
+      maxVal: '',
+      regex: '',
+      type: ''
+    }
+  }
+
+  function addInput() {
+    if (stagingInput.value) {
+      inputs.value.push(stagingInput.value)
+      resetInput()
+    }
+  }
+
+  return {
+    name,
+    description,
+    userAccounts,
+    userLogins,
+    developerAccounts,
+    developerLogins,
+    inputs,
+    batchScript,
+    stagingInput,
+    resetTemplate,
+    resetInput,
+    addInput
+  }
+})
