443ebf9cc2fa84602e21fe4daddc56b53b4b18c3
Florian Lespérant <florian.lesperant@rackslab.io>
2024-07-31 17:48:27 +0200

    feat(front): Redirect when the form is validated
    - Add router-link to `templates`
    - Reset template form and input form when template created.

--- frontend/src/views/EditTemplateView.vue
+++ frontend/src/views/EditTemplateView.vue
@@ -615,13 +615,13 @@ onMounted(async () => {
               </button></router-link
             >
 
-            <button
-              @click="createTemplate()"
+            <router-link :to="{name: 'templates'}"><button
+              @click="editTemplate()"
               type="button"
               class="mb-16 ml-5 mt-8 inline-flex w-24 justify-center gap-x-2 rounded-md bg-slurmweb px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-slurmweb-dark focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-slurmweb-dark"
             >
               Save
-            </button>
+            </button></router-link>
           </div>
           <div>{{ errorMessage }}</div>
         </div>
