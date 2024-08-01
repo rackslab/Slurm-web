52cc6a49712f571764457d6a327fe58fb4017526
Florian Lespérant <florian.lesperant@rackslab.io>
2024-08-01 16:15:32 +0200

    feat(gateway): Add new POST route edit-template

--- slurmweb/apps/gateway.py
+++ slurmweb/apps/gateway.py
@@ -74,6 +74,11 @@ class SlurmwebAppGateway(SlurmwebWebApp, RFLTokenizedWebApp):
             views.create_template,
             methods=["POST"],
         ),
+        SlurmwebAppRoute(
+            "/api/agents/<cluster>/edit-template",
+            views.edit_template,
+            methods=["POST"],
+        ),
     }
 
     def _agent_info(self, url: str) -> SlurmwebAgent:
