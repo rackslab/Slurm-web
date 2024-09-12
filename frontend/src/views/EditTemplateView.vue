f2d893ce1e6ae5293d429ffc3d59fb988e942f97
Florian Lespérant <florian.lesp@gmail.com>
2024-09-12 15:23:55 +0200

    fix(front): reset input when add input btn clicked
    instead of resetting the input form when validated,
    reset when user clicks on the add input button

--- frontend/src/views/EditTemplateView.vue
+++ frontend/src/views/EditTemplateView.vue
@@ -190,6 +190,7 @@ onMounted(async () => {
           <InputsTable />
           <router-link :to="{ name: 'create-input', params: { createOrEditInput: 'edit' } }"
             ><button
+              @click="templateStore.resetInput()"
               type="button"
               class="my-5 inline-flex items-center gap-x-2 rounded-md bg-slurmweb px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-slurmweb-dark focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-slurmweb-dark"
             >
