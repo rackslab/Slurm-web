4f435649bc84b03123e93f7becd762d00a0c8cad
Florian Lespérant <florian.lesp@gmail.com>
2024-09-20 16:38:29 +0200

    feat(front): create some tests for TemplatesView

--- frontend/src/views/TemplatesView.spec.ts
+++ frontend/src/views/TemplatesView.spec.ts
@@ -4,6 +4,7 @@ import { runtimeConfiguration } from '@/plugins/runtimeConfiguration'
 import { httpPlugin } from '@/plugins/http'
 import { setActivePinia, createPinia } from 'pinia'
 import TemplatesView from './TemplatesView.vue'
+import ClusterMainLayout from '@/components/ClusterMainLayout.vue'
 
 describe('TemplatesView.vue', () => {
   // Basic runtime configuration
@@ -23,7 +24,56 @@ describe('TemplatesView.vue', () => {
      */
     setActivePinia(createPinia())
   })
-  test('should display title', async () => {
+
+  test('should display the breadcrumb with the right titles and route', async () => {
+    const wrapper = mount(TemplatesView, {
+      props: {
+        cluster: 'foo'
+      }
+    })
+
+    const clusterMaintLayout = wrapper.getComponent(ClusterMainLayout)
+    expect(clusterMaintLayout.props().breadcrumb).toEqual([
+      { title: 'Jobs', routeName: 'jobs' },
+      { title: 'Templates' }
+    ])
+  })
+
+  test('should display back to jobs button', async () => {
+    const wrapper = mount(TemplatesView, {
+      props: {
+        cluster: 'foo'
+      }
+    })
+
+    const backButton = wrapper.get('button#backBtn')
+    expect(backButton.text()).toContain('Back to jobs')
+  })
+
+  /*test('back btn should redirect to JobsView', async () => {
+    const wrapper = mount(TemplatesView, {
+      props: {
+        cluster: 'foo',
+        to: 'test'
+      },
+    })
+
+  })*/
+
+  test('should display title and description of the view', async () => {
+    const wrapper = mount(TemplatesView, {
+      props: {
+        cluster: 'foo'
+      }
+    })
+
+    const title = wrapper.get('p#title')
+    const description = wrapper.get('p#description')
+    expect(title.text()).toBe('Templates')
+    expect(description.text()).toBe('Edit or create jobs templates')
+  })
+
+  test('should display create new template button', async () => {
     const wrapper = mount(TemplatesView, {
       props: {
         cluster: 'foo'
@@ -33,4 +83,12 @@ describe('TemplatesView.vue', () => {
     const button = wrapper.get('button#create')
     expect(button.get('span').text()).toBe('Create new template')
   })
+
+  /*test('should redirect to CreateTemplateView', async () => {
+    const wrapper = mount(TemplatesView, {
+      props: {
+        cluster: 'foo'
+      }
+    })
+  })*/
 })
