32289f3dadd86cb50128a2a0469d71ba7cb85fb1
Rémi Palancher <remi@rackslab.io>
2024-10-09 10:13:48 +0200

    tests(agent): test SlurmrestdFilteredCached

--- slurmweb/tests/test_slurmrestd.py
+++ slurmweb/tests/test_slurmrestd.py
@@ -12,13 +12,15 @@ import os
 import requests
 
 from rfl.settings import RuntimeSettings
-from slurmweb.slurmrestd import Slurmrestd, SlurmrestdFiltered
+from slurmweb.slurmrestd import Slurmrestd, SlurmrestdFiltered, SlurmrestdFilteredCached
 from slurmweb.slurmrestd.errors import (
     SlurmrestConnectionError,
     SlurmrestdInternalError,
     SlurmrestdInvalidResponseError,
     SlurmrestdNotFoundError,
 )
+from slurmweb.cache import CachingService
+from slurmweb.errors import SlurmwebCacheError
 
 from .utils import (
     all_slurm_versions,
@@ -31,6 +33,13 @@ class TestSlurmrestdBase(unittest.TestCase):
 
     def mock_slurmrestd_responses(self, slurm_version, assets):
         return mock_slurmrestd_responses(self.slurmrestd, slurm_version, assets)
+
+    def load_agent_settings_definition(self):
+        return RuntimeSettings.yaml_definition(
+            os.path.join(
+                os.path.dirname(__file__), "..", "..", "conf", "vendor", "agent.yml"
+            )
+        )
 
 
 class TestSlurmrestd(TestSlurmrestdBase):
@@ -219,11 +228,7 @@ class TestSlurmrestd(TestSlurmrestdBase):
 
 class TestSlurmrestdFiltered(TestSlurmrestdBase):
     def setUp(self):
-        self.settings = RuntimeSettings.yaml_definition(
-            os.path.join(
-                os.path.dirname(__file__), "..", "..", "conf", "vendor", "agent.yml"
-            )
-        )
+        self.settings = self.load_agent_settings_definition()
         self.slurmrestd = SlurmrestdFiltered(
             Path("/dev/null"), "1.0.0", self.settings.filters
         )
@@ -363,3 +368,96 @@ class TestSlurmrestdFiltered(TestSlurmrestdBase):
             # Check arbitrary key has been filtered out.
             self.assertIn("usage_threshold", asset[idx])
             self.assertNotIn("usage_threshold", qos[idx])
+
+
+class TestSlurmrestdFilteredCached(TestSlurmrestdBase):
+    def setUp(self):
+        self.settings = self.load_agent_settings_definition()
+        self.settings.cache.enabled = True
+        self.cache = CachingService(
+            self.settings.cache.host,
+            self.settings.cache.port,
+            self.settings.cache.password,
+        )
+        self.slurmrestd = SlurmrestdFilteredCached(
+            Path("/dev/null"),
+            "1.0.0",
+            self.settings.filters,
+            self.settings.cache,
+            self.cache,
+        )
+
+    @all_slurm_versions
+    def test_not_in_cache(self, slurm_version):
+        try:
+            [asset] = self.mock_slurmrestd_responses(
+                slurm_version, [("slurm-jobs", "jobs")]
+            )
+        except SlurmwebAssetUnavailable:
+            return
+        self.slurmrestd.service.get = mock.Mock(return_value=None)
+        self.slurmrestd.service.put = mock.Mock()
+        jobs = self.slurmrestd.jobs()
+        for idx in range(len(jobs)):
+            self.assertEqual(jobs[idx]["job_id"], asset[idx]["job_id"])
+        # Check SlurmrestdFilteredCached has tried to get jobs from cache
+        self.slurmrestd.service.get.assert_called_once_with("jobs")
+        # Check SlurmrestdFilteredCached has up jobs in cache with corresponding
+        # expiration timeout.
+        self.slurmrestd.service.put.assert_called_once_with(
+            "jobs", jobs, self.settings.cache.jobs
+        )
+
+    @all_slurm_versions
+    def test_in_cache(self, slurm_version):
+        try:
+            [asset] = self.mock_slurmrestd_responses(
+                slurm_version, [("slurm-jobs", "jobs")]
+            )
+        except SlurmwebAssetUnavailable:
+            return
+        self.slurmrestd.service.get = mock.Mock(return_value=asset)
+        self.slurmrestd.service.put = mock.Mock()
+        jobs = self.slurmrestd.jobs()
+        for idx in range(len(jobs)):
+            self.assertEqual(jobs[idx]["job_id"], asset[idx]["job_id"])
+        # Check SlurmrestdFilteredCached has tried to get jobs from cache.
+        self.slurmrestd.service.get.assert_called_once_with("jobs")
+        # Check SlurmrestdFilteredCached has not put jobs again in cache.
+        self.slurmrestd.service.put.assert_not_called()
+
+    @all_slurm_versions
+    def test_cache_get_error(self, slurm_version):
+        try:
+            [asset] = self.mock_slurmrestd_responses(
+                slurm_version, [("slurm-jobs", "jobs")]
+            )
+        except SlurmwebAssetUnavailable:
+            return
+        # Check behaviour when SlurmwebCacheError in raised at get()
+        self.slurmrestd.service.get = mock.Mock(
+            side_effect=SlurmwebCacheError("fake cache error")
+        )
+        self.slurmrestd.service.put = mock.Mock()
+        with self.assertRaisesRegex(SlurmwebCacheError, "^fake cache error$"):
+            self.slurmrestd.jobs()
+        self.slurmrestd.service.get.assert_called_once_with("jobs")
+        self.slurmrestd.service.put.assert_not_called()
+
+    @all_slurm_versions
+    def test_cache_put_error(self, slurm_version):
+        try:
+            [asset] = self.mock_slurmrestd_responses(
+                slurm_version, [("slurm-jobs", "jobs")]
+            )
+        except SlurmwebAssetUnavailable:
+            return
+        # Check behaviour when SlurmwebCacheError in raised at put()
+        self.slurmrestd.service.get = mock.Mock(return_value=None)
+        self.slurmrestd.service.put = mock.Mock(
+            side_effect=SlurmwebCacheError("fake cache error")
+        )
+        with self.assertRaisesRegex(SlurmwebCacheError, "^fake cache error$"):
+            self.slurmrestd.jobs()
+        self.slurmrestd.service.get.assert_called_once_with("jobs")
+        self.slurmrestd.service.put.assert_called_once()
