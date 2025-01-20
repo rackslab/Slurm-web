421442dd967ae7e7732cf7d660b7e40354758e6f
Rémi Palancher <remi@rackslab.io>
2025-01-20 09:08:49 +0100

    refactor(front): remove firefox blurry workaround
    Remove workaround for Firefox to avoid blurry lines in racks canvas,
    fixed in Firefox ESR >= 128 and Firefox >= 133. For reference, see:
    https://bugzilla.mozilla.org/show_bug.cgi?id=1847681

--- frontend/src/components/resources/ResourcesCanvas.vue
+++ frontend/src/components/resources/ResourcesCanvas.vue
@@ -31,7 +31,6 @@ const props = defineProps({
   }
 })
 
-const isFirefox = navigator.userAgent.includes('Firefox')
 const emit = defineEmits(['imageSize'])
 
 const gateway = useGatewayAPI()
@@ -158,8 +157,8 @@ async function updateCanvas(fullUpdate: boolean = true) {
       // Draw all nodes using their coordinates
       for (const [nodeName, nodeCoordinates] of Object.entries(coordinates)) {
         const nodePath = new Path2D()
-        const node_x = nodeCoordinates[0] + x_shift + 1 - (isFirefox ? 0 : 0.5)
-        const node_y = nodeCoordinates[1] + y_shift - (isFirefox ? 0 : 0.5)
+        const node_x = nodeCoordinates[0] + x_shift + 0.5
+        const node_y = nodeCoordinates[1] + y_shift - 0.5
         nodePath.rect(node_x, node_y, nodeCoordinates[2] - 1, nodeCoordinates[3])
 
         if (!inSelectedNodes(nodeName)) {
@@ -180,8 +179,8 @@ async function updateCanvas(fullUpdate: boolean = true) {
               ctx.lineWidth = 2
             }
             ctx.strokeRect(
-              nodeCoordinates[0] + x_shift + ctx.lineWidth / 2 + 1 - (isFirefox ? 0 : 0.5),
-              nodeCoordinates[1] + y_shift + ctx.lineWidth / 2 - (isFirefox ? 0 : 0.5),
+              nodeCoordinates[0] + x_shift + ctx.lineWidth / 2 + 0.5,
+              nodeCoordinates[1] + y_shift + ctx.lineWidth / 2 - 0.5,
               nodeCoordinates[2] - ctx.lineWidth - 1,
               nodeCoordinates[3] - ctx.lineWidth
             )
