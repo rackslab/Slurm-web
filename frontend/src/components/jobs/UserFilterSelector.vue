2c10b9c9fb70d47c04dd81a13440d69e36842708
Rémi Palancher <remi@rackslab.io>
2025-04-08 12:06:07 +0200

    chore(front): some tailwindcss v4 adjustments
    Some classes have not been properly migrated with automatic updater.

--- frontend/src/components/jobs/UserFilterSelector.vue
+++ frontend/src/components/jobs/UserFilterSelector.vue
@@ -66,7 +66,7 @@ const { data } = useGatewayDataGetter<UserDescription[]>('users')
 
       <ComboboxOptions
         v-if="filteredUsers.length > 0"
-        class="ring-opacity-5 absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black focus:outline-hidden sm:text-sm"
+        class="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm"
       >
         <ComboboxOption
           v-for="user in filteredUsers"
