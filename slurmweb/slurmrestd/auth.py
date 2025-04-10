b57125fa89004bf5276db357004faa6d2ecd2e04
Rémi Palancher <remi@rackslab.io>
2025-04-10 16:16:07 +0200

    feat(agent): check slurmrestd JWT format and exp
    Before trying to use it to authenticate on slurmrestd, check the JWT set
    in configuration has a valid format and is not expired. Otherwise, raise
    SlurmwebConfigurationError to make the agent report critical error and
    fail.

--- slurmweb/slurmrestd/auth.py
+++ slurmweb/slurmrestd/auth.py
@@ -6,6 +6,11 @@
 
 import typing as t
 
+from rfl.authentication.jwt import jwt_validate_expiration
+from rfl.authentication.errors import JWTDecodeError
+
+from ..errors import SlurmwebConfigurationError
+
 
 class SlurmrestdAuthentifier:
     def __init__(
@@ -17,6 +22,14 @@ class SlurmrestdAuthentifier:
         self.method = method
         self.jwt_user = jwt_user
         self.jwt_token = jwt_token
+        # Check token validity (format and expiration).
+        if self.jwt_token:
+            try:
+                jwt_validate_expiration(self.jwt_token)
+            except JWTDecodeError as err:
+                raise SlurmwebConfigurationError(
+                    f"Invalid slurmrestd JWT: {err}"
+                ) from err
 
     def headers(self):
         if self.method == "local":
