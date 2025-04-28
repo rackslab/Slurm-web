b041df675f1faede0065677a5aad33da0838f67e
Rémi Palancher <remi@rackslab.io>
2025-04-28 21:28:38 +0200

    Revert "chore(front): adopt @tailwindcss/vite plugin"
    This reverts commit 78a91b3ba300f848c4aea7001694159358bd8763.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,14 +9,12 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
-import tailwindcss from "@tailwindcss/vite";
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    tailwindcss(),
   ],
   define: {
     // disable hydration mismatch details in production build
