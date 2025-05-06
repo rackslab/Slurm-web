dcedfa6591911590e06f3b375dcace8124314cbb
Rémi Palancher <remi@rackslab.io>
2025-05-06 17:01:18 +0200

    chore(dev): move DevelopmentHostCluster in lib

--- dev/crawler/lib.py
+++ dev/crawler/lib.py
@@ -6,11 +6,16 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+from __future__ import annotations
 import sys
 import typing as t
 import json
 import socket
 from pathlib import Path
+import shlex
+import urllib
+import time
+import random
 import logging
 
 import requests
@@ -22,6 +27,11 @@ from rfl.settings.errors import (
     SettingsOverrideError,
     SettingsSiteLoaderError,
 )
+
+from slurmweb.slurmrestd.unix import SlurmrestdUnixAdapter
+
+if t.TYPE_CHECKING:
+    from slurmweb.slurmrestd.auth import SlurmrestdAuthentifier
 
 ASSETS = Path(__file__).parent.resolve() / ".." / ".." / "tests" / "assets"
 
@@ -105,6 +115,137 @@ class DevelopmentHostClient:
 
     def exec(self, cmd: str):
         return self._client.exec_command(cmd)
+
+
+class DevelopmentHostCluster:
+    def __init__(
+        self,
+        dev_host: DevelopmentHostClient,
+        name: str,
+        uri: urllib.parse.ParseResult,
+        auth: SlurmrestdAuthentifier,
+    ):
+        self.dev_host = dev_host
+        self.name = name
+        self.api = "0.0.40"
+        self.auth = auth
+        self.session = requests.Session()
+
+        if uri.scheme == "unix":
+            self.prefix = "http+unix://slurmrestd"
+            self.session.mount(self.prefix, SlurmrestdUnixAdapter(uri.path))
+        else:
+            self.prefix = uri.geturl()
+
+    def query_slurmrestd(self, query: str, headers: dict[str, str] | None = None):
+        """Send GET HTTP request to slurmrestd and return JSON result. Raise
+        RuntimeError in case of connection error or not JSON result."""
+        if headers is None:
+            headers = self.auth.headers()
+        try:
+            response = self.session.get(f"{self.prefix}/{query}", headers=headers)
+        except requests.exceptions.ConnectionError as err:
+            raise RuntimeError(f"Unable to connect to slurmrestd: {err}")
+
+        return response.text, response.headers.get("content-type"), response.status_code
+
+    def query_slurmrestd_json(self, query: str, headers: dict[str, str] | None = None):
+        """Send GET HTTP request to slurmrestd and return JSON result. Raise
+        RuntimeError in case of connection error or not JSON result."""
+        if headers is None:
+            headers = self.auth.headers()
+        try:
+            response = self.session.get(f"{self.prefix}/{query}", headers=headers)
+        except requests.exceptions.ConnectionError as err:
+            raise RuntimeError(f"Unable to connect to slurmrestd: {err}")
+
+        if response.headers.get("content-type") != "application/json":
+            raise CrawlerError(
+                f"Unexpected content-type for slurmrestd query {query}: "
+                f"{response.headers.get('content-type')}"
+            )
+
+        if response.status_code != 200:
+            raise CrawlerError(
+                f"Unexpected status code for slurmrestd query {query}: "
+                f"{response.status_code}"
+            )
+
+        return response.json()
+
+    def submit(self, user: str, args: list[str], wait_running: bool = True) -> int:
+        _, stdout, stderr = self.dev_host.exec(
+            shlex.join(
+                [
+                    "firehpc",
+                    "ssh",
+                    f"{user}@login.{self.name}",
+                    "--",
+                    "sbatch",
+                    "--wrap",
+                    "'sleep 30'",
+                ]
+                + args
+            )
+        )
+        output = stdout.read().decode()
+        errors = stderr.read().decode()
+        if not output.startswith("Submitted batch job"):
+            raise CrawlerError(f"Unable to submit batch job on GPU: {output}/{errors}")
+        job_id = int(output.split(" ")[3])
+        if wait_running:
+            max_tries = 3
+            for idx in range(max_tries):
+                job = self.query_slurmrestd_json(f"/slurm/v{self.api}/job/{job_id}")
+                if "RUNNING" in job["jobs"][0]["job_state"]:
+                    break
+                if idx == max_tries - 1:
+                    raise CrawlerError(f"Unable to get job {job_id} in RUNNING state")
+                else:
+                    time.sleep(1)
+        return job_id
+
+    def cancel(self, user: str, job_id: int) -> None:
+        self.dev_host.exec(
+            shlex.join(
+                [
+                    "firehpc",
+                    "ssh",
+                    f"{user}@login.{self.name}",
+                    "--",
+                    "scancel",
+                    str(job_id),
+                ]
+            )
+        )
+
+    def job_nodes(self, job_id: int) -> list[str]:
+        job = self.query_slurmrestd_json(f"/slurm/v{self.api}/job/{job_id}")
+        return [
+            allocated_node["nodename"]
+            for allocated_node in job["jobs"][0]["job_resources"]["allocated_nodes"]
+        ]
+
+    def wait_idle(self, node_name: str) -> None:
+        max_tries = 3
+        for idx in range(max_tries):
+            node = self.query_slurmrestd_json(f"/slurm/v{self.api}/node/{node_name}")
+            if not busy_node(node["nodes"][0]):
+                break
+            if idx == max_tries - 1:
+                raise CrawlerError(f"Unable to get job {node} IDLE")
+            else:
+                time.sleep(1)
+
+    def pick_user(self):
+        """Return name of a user in admin group for the given cluster."""
+
+        _, stdout, _ = self.dev_host.exec(
+            shlex.join(["firehpc", "status", "--cluster", self.name, "--json"])
+        )
+        cluster_status = json.loads(stdout.read())
+
+        return random.choice(cluster_status["users"])["login"]
 
 
 class CrawlerError(Exception):
