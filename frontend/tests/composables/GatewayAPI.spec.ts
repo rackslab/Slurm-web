b0c6e3f5929ebc6364a62f5332ce9cfa2641eac6
Rémi Palancher <remi@rackslab.io>
2025-05-12 21:55:26 +0200

    tests(front): cover job gpu functions

--- frontend/tests/composables/GatewayAPI.spec.ts
+++ frontend/tests/composables/GatewayAPI.spec.ts
@@ -1,11 +1,27 @@
 import { describe, expect, test } from 'vitest'
 import {
   compareClusterJobSortOrder,
+  jobResourcesTRES,
+  jobAllocatedGPU,
+  jobRequestedGPU,
+  jobResourcesGPU,
   getMBHumanUnit,
   getNodeGPUFromGres,
   getNodeGPU
 } from '@/composables/GatewayAPI'
 import jobs from '../assets/jobs.json'
+import jobPending from '../assets/job-pending.json'
+import jobGpuArchived from '../assets/job-gpus-archived.json'
+import jobGpuCompleted from '../assets/job-gpus-completed.json'
+import jobGpuPending from '../assets/job-gpus-pending.json'
+import jobGpuRunning from '../assets/job-gpus-running.json'
+import jobGpuGres from '../assets/job-gpus-gres.json'
+import jobGpuMultiNodes from '../assets/job-gpus-multi-nodes.json'
+import jobGpuType from '../assets/job-gpus-type.json'
+import jobGpuMultiTypes from '../assets/job-gpus-multi-types.json'
+import jobGpuPerNode from '../assets/job-gpus-per-node.json'
+import jobGpuPerSocket from '../assets/job-gpus-per-socket.json'
+import jobGpuPerTask from '../assets/job-gpus-per-task.json'
 import nodeWithGpusAllocated from '../assets/node-with-gpus-allocated.json'
 import nodeWithGpusIdle from '../assets/node-with-gpus-idle.json'
 import nodeWithGpusMixed from '../assets/node-with-gpus-mixed.json'
@@ -210,6 +226,322 @@ describe('compareClusterJobSorter', () => {
     // A == B
     expect(compareClusterJobSortOrder(jobA, jobB, 'resources', 'asc')).toBe(0)
     expect(compareClusterJobSortOrder(jobA, jobB, 'resources', 'desc')).toBe(0)
+  })
+})
+
+describe('jobResourcesTRES', () => {
+  test('basic TRES', () => {
+    const job = jobPending
+    job.tres.requested = [
+      {
+        count: 128,
+        id: 1,
+        name: '',
+        type: 'cpu'
+      },
+      {
+        count: 65536,
+        id: 2,
+        name: '',
+        type: 'mem'
+      },
+      {
+        count: 1,
+        id: 4,
+        name: '',
+        type: 'node'
+      },
+      {
+        count: 128,
+        id: 5,
+        name: '',
+        type: 'billing'
+      }
+    ]
+    expect(jobResourcesTRES(job.tres.requested)).toStrictEqual({ node: 1, cpu: 128, memory: 65536 })
+  })
+  test('empty TRES', () => {
+    const job = jobPending
+    job.tres.requested = []
+    expect(jobResourcesTRES(job.tres.requested)).toStrictEqual({ node: -1, cpu: -1, memory: -1 })
+  })
+})
+
+describe('jobAllocatedGPU', () => {
+  // test specific values
+  test('empty GRES', () => {
+    const job = { ...jobs[0] }
+    job.gres_detail = []
+    expect(jobAllocatedGPU(job)).toBe(-1)
+  })
+  test('simple GRES', () => {
+    const job = { ...jobs[0] }
+    job.gres_detail = ['gpu:4']
+    expect(jobAllocatedGPU(job)).toBe(4)
+  })
+  test('mixed GRES', () => {
+    const job = { ...jobs[0] }
+    job.gres_detail = ['license:1,gpu:2']
+    expect(jobAllocatedGPU(job)).toBe(2)
+  })
+  test('GRES with prefix', () => {
+    const job = { ...jobs[0] }
+    job.gres_detail = ['gres/gpu:4']
+    expect(jobAllocatedGPU(job)).toBe(4)
+  })
+  test('GRES with model', () => {
+    const job = { ...jobs[0] }
+    job.gres_detail = ['gpu:h200:8']
+    expect(jobAllocatedGPU(job)).toBe(8)
+  })
+  test('GRES with model and prefix', () => {
+    const job = { ...jobs[0] }
+    job.gres_detail = ['gres/gpu:h200:6']
+    expect(jobAllocatedGPU(job)).toBe(6)
+  })
+  test('GRES with index', () => {
+    const job = { ...jobs[0] }
+    job.gres_detail = ['gpu:2(IDX:0-1)']
+    expect(jobAllocatedGPU(job)).toBe(2)
+  })
+  test('GRES with model and index', () => {
+    const job = { ...jobs[0] }
+    job.gres_detail = ['gpu:h100:2(IDX:2-3)']
+    expect(jobAllocatedGPU(job)).toBe(2)
+  })
+  test('multiple GRES with model and index', () => {
+    const job = { ...jobs[0] }
+    job.gres_detail = ['gpu:h100:2(IDX:2-3)', 'gpu:h100:4(IDX:0-3)']
+    expect(jobAllocatedGPU(job)).toBe(6)
+  })
+  // test with assets
+  test('archived job', () => {
+    const job = { ...jobGpuArchived }
+    expect(jobAllocatedGPU(job)).toBe(-1)
+  })
+  test('completed job', () => {
+    const job = { ...jobGpuCompleted }
+    expect(jobAllocatedGPU(job)).toBe(-1)
+  })
+  test('pending job', () => {
+    const job = { ...jobGpuPending }
+    expect(jobAllocatedGPU(job)).toBe(-1)
+  })
+  test('running job', () => {
+    const job = { ...jobGpuRunning }
+    expect(jobAllocatedGPU(job)).toBeGreaterThan(0)
+  })
+  test('running gres', () => {
+    const job = { ...jobGpuGres }
+    expect(jobAllocatedGPU(job)).toBeGreaterThan(0)
+  })
+  test('running multi nodes', () => {
+    const job = { ...jobGpuMultiNodes }
+    expect(jobAllocatedGPU(job)).toBeGreaterThan(0)
+  })
+  test('running type', () => {
+    const job = { ...jobGpuType }
+    expect(jobAllocatedGPU(job)).toBeGreaterThan(0)
+  })
+  test('running multi types', () => {
+    const job = { ...jobGpuMultiTypes }
+    expect(jobAllocatedGPU(job)).toBeGreaterThan(0)
+  })
+  test('running gpu per node', () => {
+    const job = { ...jobGpuPerNode }
+    expect(jobAllocatedGPU(job)).toBeGreaterThan(0)
+  })
+  test('running gpu per socket', () => {
+    const job = { ...jobGpuPerSocket }
+    expect(jobAllocatedGPU(job)).toBeGreaterThan(0)
+  })
+  test('running gpu per task', () => {
+    const job = { ...jobGpuPerTask }
+    expect(jobAllocatedGPU(job)).toBeGreaterThan(0)
+  })
+})
+
+describe('jobRequestedGPU', () => {
+  // tests with specific values
+  test('empty requested GRES', () => {
+    const job = { ...jobs[0] }
+    job.tres_per_job = ''
+    job.tres_per_node = ''
+    job.tres_per_socket = ''
+    job.tres_per_task = ''
+    expect(jobRequestedGPU(job)).toStrictEqual({ count: 0, reliable: true })
+  })
+  test('simple GRES requested per job', () => {
+    const job = { ...jobs[0] }
+    job.tres_per_job = 'gres/gpu:2'
+    job.tres_per_node = ''
+    job.tres_per_socket = ''
+    job.tres_per_task = ''
+    expect(jobRequestedGPU(job)).toStrictEqual({ count: 2, reliable: true })
+  })
+  test('simple GRES requested per job with equal sign', () => {
+    const job = { ...jobs[0] }
+    job.tres_per_job = 'gres/gpu=2'
+    job.tres_per_node = ''
+    job.tres_per_socket = ''
+    job.tres_per_task = ''
+    expect(jobRequestedGPU(job)).toStrictEqual({ count: 2, reliable: true })
+  })
+  test('multiple GRES requested per job', () => {
+    const job = { ...jobs[0] }
+    job.tres_per_job = 'gres/license:1,gres/gpu:4'
+    job.tres_per_node = ''
+    job.tres_per_socket = ''
+    job.tres_per_task = ''
+    expect(jobRequestedGPU(job)).toStrictEqual({ count: 4, reliable: true })
+  })
+  test('multiple GRES with gpu type with equal sign', () => {
+    const job = { ...jobs[0] }
+    job.tres_per_job = 'gres/gpu:h100=6'
+    job.tres_per_node = ''
+    job.tres_per_socket = ''
+    job.tres_per_task = ''
+    expect(jobRequestedGPU(job)).toStrictEqual({ count: 6, reliable: true })
+  })
+  test('multiple GRES with gpu type per job', () => {
+    const job = { ...jobs[0] }
+    job.tres_per_job = 'gres/license:1,gres/gpu:h100:6'
+    job.tres_per_node = ''
+    job.tres_per_socket = ''
+    job.tres_per_task = ''
+    expect(jobRequestedGPU(job)).toStrictEqual({ count: 6, reliable: true })
+  })
+  test('multiple GRES with multiple type per job', () => {
+    const job = { ...jobs[0] }
+    job.tres_per_job = 'gres/license:1,gres/gpu:h100:2,gres/gpu:h200:6'
+    job.tres_per_node = ''
+    job.tres_per_socket = ''
+    job.tres_per_task = ''
+    expect(jobRequestedGPU(job)).toStrictEqual({ count: 8, reliable: true })
+  })
+  test('simple GRES requested per node', () => {
+    const job = { ...jobs[0] }
+    job.tres_per_job = ''
+    job.tres_per_node = 'gres/gpu:2'
+    job.tres_per_socket = ''
+    job.tres_per_task = ''
+    job.node_count.number = 4
+    expect(jobRequestedGPU(job)).toStrictEqual({ count: 8, reliable: true })
+  })
+  test('simple GRES requested per socket', () => {
+    const job = { ...jobs[0] }
+    job.tres_per_job = ''
+    job.tres_per_node = ''
+    job.tres_per_socket = 'gres/gpu:2'
+    job.tres_per_task = ''
+    job.node_count.number = 4
+    job.sockets_per_node.set = true
+    job.sockets_per_node.number = 2
+    expect(jobRequestedGPU(job)).toStrictEqual({ count: 16, reliable: false })
+  })
+  test('simple GRES requested per task', () => {
+    const job = { ...jobs[0] }
+    job.tres_per_job = ''
+    job.tres_per_node = ''
+    job.tres_per_socket = ''
+    job.tres_per_task = 'gres/gpu:2'
+    job.tasks.number = 4
+    expect(jobRequestedGPU(job)).toStrictEqual({ count: 8, reliable: true })
+  })
+  // tests with assets
+  test('archived job', () => {
+    const job = { ...jobGpuArchived }
+    expect(jobRequestedGPU(job)).toStrictEqual({ count: 0, reliable: true })
+  })
+  test('completed job', () => {
+    const job = { ...jobGpuCompleted }
+    const gpu = jobRequestedGPU(job)
+    expect(gpu.count).toBeGreaterThan(0)
+    expect(gpu.reliable).toBeTruthy()
+  })
+  test('pending job', () => {
+    const job = { ...jobGpuPending }
+    const gpu = jobRequestedGPU(job)
+    expect(gpu.count).toBeGreaterThan(0)
+    expect(gpu.reliable).toBeTruthy()
+  })
+  test('running job', () => {
+    const job = { ...jobGpuPending }
+    const gpu = jobRequestedGPU(job)
+    expect(gpu.count).toBeGreaterThan(0)
+    expect(gpu.reliable).toBeTruthy()
+  })
+  test('running gres', () => {
+    const job = { ...jobGpuGres }
+    const gpu = jobRequestedGPU(job)
+    expect(gpu.count).toBeGreaterThan(0)
+    expect(gpu.reliable).toBeTruthy()
+  })
+  test('running multi nodes', () => {
+    const job = { ...jobGpuMultiNodes }
+    const gpu = jobRequestedGPU(job)
+    expect(gpu.count).toBeGreaterThan(0)
+    expect(gpu.reliable).toBeTruthy()
+  })
+  test('running type', () => {
+    const job = { ...jobGpuType }
+    const gpu = jobRequestedGPU(job)
+    expect(gpu.count).toBeGreaterThan(0)
+    expect(gpu.reliable).toBeTruthy()
+  })
+  test('running multi types', () => {
+    const job = { ...jobGpuMultiTypes }
+    const gpu = jobRequestedGPU(job)
+    expect(gpu.count).toBeGreaterThan(0)
+    expect(gpu.reliable).toBeTruthy()
+  })
+  test('running GPU per node', () => {
+    const job = { ...jobGpuPerNode }
+    const gpu = jobRequestedGPU(job)
+    expect(gpu.count).toBeGreaterThan(0)
+    expect(gpu.reliable).toBeTruthy()
+  })
+  test('running GPU per socket', () => {
+    const job = { ...jobGpuPerSocket }
+    const gpu = jobRequestedGPU(job)
+    expect(gpu.count).toBeGreaterThan(0)
+    expect(gpu.reliable).toBeFalsy()
+  })
+  test('running GPU per task', () => {
+    const job = { ...jobGpuPerTask }
+    const gpu = jobRequestedGPU(job)
+    expect(gpu.count).toBeGreaterThan(0)
+    expect(gpu.reliable).toBeTruthy()
+  })
+})
+
+describe('jobResourcesGPU', () => {
+  test('empty GRES', () => {
+    const job = { ...jobs[0] }
+    job.gres_detail = []
+    job.tres_per_job = ''
+    job.tres_per_node = ''
+    job.tres_per_socket = ''
+    job.tres_per_task = ''
+    expect(jobResourcesGPU(job)).toStrictEqual({ count: 0, reliable: true })
+  })
+  test('with requested GRES', () => {
+    const job = { ...jobs[0] }
+    job.gres_detail = []
+    job.tres_per_job = 'gres/gpu:4'
+    job.tres_per_node = ''
+    job.tres_per_socket = ''
+    job.tres_per_task = ''
+    expect(jobResourcesGPU(job)).toStrictEqual({ count: 4, reliable: true })
+  })
+  test('with allocated GRES', () => {
+    const job = { ...jobs[0] }
+    job.gres_detail = ['gpu:2']
+    job.tres_per_job = 'gres/gpu:4'
+    job.tres_per_node = ''
+    job.tres_per_socket = ''
+    job.tres_per_task = ''
+    expect(jobResourcesGPU(job)).toStrictEqual({ count: 2, reliable: true })
   })
 })
 
