fee4e041e364e843eba375afacea61bc6b78f095
Rémi Palancher <remi@rackslab.io>
2025-05-13 10:10:44 +0200

    feat(front): display GPU metrics in dashboard
    Add buttons to display GPU status metrics in dashboard, in alternative
    to nodes and cores metrics.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -16,6 +16,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
     page.
   - Display number of GPU requested or allocated in jobs list.
   - Display number of GPU requested and allocated in job details page.
+  - Add buttons to display GPU status metrics in dashboard.
 - agent:
   - Add total quantity of memory and number of gpus in cluster stats response.
   - Add metrics for GPU by state and total number of GPU.
--- frontend/src/stores/runtime/dashboard.ts
+++ frontend/src/stores/runtime/dashboard.ts
@@ -14,27 +14,34 @@ import type { MetricRange } from '@/composables/GatewayAPI'
  * Dashboard view settings
  */
 
+const chartResourcesTypes = ['nodes', 'cores', 'gpus'] as const
+export type ChartResourcesType = (typeof chartResourcesTypes)[number]
+
+export function isChartResourcesType(value: unknown): value is ChartResourcesType {
+  return typeof value === 'string' && (chartResourcesTypes as readonly string[]).indexOf(value) >= 0
+}
+
 export interface DashboardQueryParameters {
   range?: string
-  cores?: boolean
+  resources?: ChartResourcesType
 }
 
 export const useDashboardRuntimeStore = defineStore('dashboardRuntime', () => {
   const range = ref<MetricRange>('hour')
-  const coresToggle = ref(false)
+  const chartResourcesType = ref<ChartResourcesType>('nodes')
   function query() {
     const result: DashboardQueryParameters = {}
     if (range.value != 'hour') {
       result.range = range.value
     }
-    if (coresToggle.value) {
-      result.cores = coresToggle.value
+    if (chartResourcesType.value != 'nodes') {
+      result.resources = chartResourcesType.value
     }
     return result
   }
   return {
     range,
-    coresToggle,
+    chartResourcesType,
     query
   }
 })
