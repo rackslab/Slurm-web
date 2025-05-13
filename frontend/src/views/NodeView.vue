2e93696f016a6eb27913850fcf5f7ff89f502f8e
Rémi Palancher <remi@rackslab.io>
2025-05-13 15:46:24 +0200

    fix(front): remove focus-visible:outline class
    Remove useless focus-visible:outline class in presence of
    focus-visible:outline-2 class on the same component.

--- frontend/src/views/NodeView.vue
+++ frontend/src/views/NodeView.vue
@@ -67,7 +67,7 @@ const gpuAllocated = computed(() => {
     <button
       @click="backToResources()"
       type="button"
-      class="bg-slurmweb hover:bg-slurmweb-dark focus-visible:outline-slurmweb-dark mt-8 mb-16 inline-flex items-center gap-x-2 rounded-md px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
+      class="bg-slurmweb hover:bg-slurmweb-dark focus-visible:outline-slurmweb-dark mt-8 mb-16 inline-flex items-center gap-x-2 rounded-md px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs focus-visible:outline-2 focus-visible:outline-offset-2"
     >
       <ChevronLeftIcon class="-ml-0.5 h-5 w-5" aria-hidden="true" />
       Back to resources
