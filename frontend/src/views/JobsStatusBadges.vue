8e41cd85d52663ae2e56b75baf430ddc616bac35
Rémi Palancher <remi@rackslab.io>
2025-05-19 16:01:21 +0200

    feat(front): add testing view for all job badges

---
+++ frontend/src/views/JobsStatusBadges.vue
@@ -0,0 +1,46 @@
+<!--
+  Copyright (c) 2025 Rackslab
+
+  This file is part of Slurm-web.
+
+  SPDX-License-Identifier: GPL-3.0-or-later
+-->
+
+<script setup lang="ts">
+import JobStatusBadge from '@/components/job/JobStatusBadge.vue'
+
+const badges = [
+  ['PENDING'],
+  ['RUNNING'],
+  ['SUSPENDED'],
+  ['COMPLETED'],
+  ['CANCELLED'],
+  ['FAILED'],
+  ['TIMEOUT'],
+  ['NODE_FAIL'],
+  ['PREEMPTED'],
+  ['BOOT_FAIL'],
+  ['DEADLINE'],
+  ['OUT_OF_MEMORY'],
+  ['RUNNING', 'COMPLETING'],
+  ['COMPLETED', 'STAGE_OUT'],
+  ['PENDING', 'CONFIGURING'],
+  ['RUNNING', 'RESIZING'],
+  ['PENDING', 'REQUEUD'],
+  ['PENDING', 'REQUEUE_FED'],
+  ['PENDING', 'REQUEUE_HOLD'],
+  ['PENDING', 'RESV_DEL_HOLD'],
+  ['FAILED', 'SPECIAL_EXIT'],
+  ['FAILED', 'STOPPED'],
+  ['FAILED', 'REVOKED'],
+  ['TIMEOUT', 'SIGNALING']
+]
+</script>
+
+<template>
+  <div class="container m-auto max-w-2xl">
+    <span v-for="badge in badges" class="m-4 mx-0.5">
+      <JobStatusBadge :status="badge" />
+    </span>
+  </div>
+</template>
