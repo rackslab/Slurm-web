2ed5d643818c6285fb2803c017aea00e4e8302dc
Rémi Palancher <remi@rackslab.io>
2025-06-11 09:18:17 +0200

    refactor(agent): introduce CacheKey
    Introduce CacheKey class to allow differentiation between actual cache
    key and cache statistics key.

--- slurmweb/cache.py
+++ slurmweb/cache.py
@@ -4,7 +4,7 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from typing import Union, Any
+import typing as t
 
 import redis
 import pickle
@@ -12,24 +12,36 @@ import pickle
 from .errors import SlurmwebCacheError
 
 
+class CacheKey:
+    def __init__(self, main: str, count: t.Optional[str] = None):
+        self.main = main
+        if count:
+            self.count = count
+        else:
+            self.count = main
+
+    def __eq__(self, other):
+        return self.main == other.main and self.count == other.count
+
+
 class CachingService:
-    def __init__(self, host: str, port: int, password: Union[str, None]):
+    def __init__(self, host: str, port: int, password: t.Union[str, None]):
         self.host = host
         self.port = port
         self.connection = redis.Redis(host=host, port=port, password=password)
 
-    def put(self, key: str, value: Any, expiration: int):
+    def put(self, key: CacheKey, value: t.Any, expiration: int):
         try:
-            self.connection.set(key, pickle.dumps(value), ex=expiration)
+            self.connection.set(key.main, pickle.dumps(value), ex=expiration)
         except (
             redis.exceptions.ConnectionError,
             redis.exceptions.ResponseError,
         ) as err:
             raise SlurmwebCacheError(str(err)) from err
 
-    def get(self, key):
+    def get(self, key: CacheKey):
         try:
-            value = self.connection.get(key)
+            value = self.connection.get(key.main)
             if value is not None:
                 value = pickle.loads(value)
             return value
