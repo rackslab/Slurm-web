rackslab/Slurm-web

commit 822d95a687114f7d3df34f54a09648eb94e9973a

chore(dev): update crawler for gpus without model

dev/crawler/lib.py
@@ -409,6 +409,25 @@ class DevelopmentHostCluster:
         )
 
 
+def get_component_response(
+    url: str,
+    query: str,
+    headers: str,
+    method: str = "GET",
+    content: t.Optional[t.Dict] = None,
+):
+    if method == "GET":
+        response = requests.get(f"{url}{query}", headers=headers)
+    elif method == "POST":
+        kwargs = {}
+        if content:
+            kwargs["json"] = content
+        response = requests.post(f"{url}{query}", headers=headers, **kwargs)
+    else:
+        raise RuntimeError(f"Unsupport request method {method}")
+    return response
+
+
 def dump_component_query(
     requests_statuses,
     url: str,
@@ -446,6 +465,19 @@ def dump_component_query(
         response = requests.post(f"{url}{query}", headers=headers, **kwargs)
     else:
         raise RuntimeError(f"Unsupport request method {method}")
+    dump_component_response(
+        requests_statuses, assets_path, asset_name, response, prettify, limit_dump
+    )
+
+
+def dump_component_response(
+    requests_statuses,
+    assets_path: Path,
+    asset_name: dict[int, str] | str,
+    response,
+    prettify: bool = True,
+    limit_dump=0,
+):
     if isinstance(asset_name, dict):
         _asset_name = asset_name[response.status_code]
     else:
@@ -510,6 +542,13 @@ class TokenizedComponentCrawler(ComponentCrawler):
         self.url = url
         self.token = token
 
+    def get_component_response(
+        self, query: str, headers: dict[str, str] | None = None, **kwargs
+    ):
+        if headers is None:
+            headers = {"Authorization": f"Bearer {self.token}"}
+        return get_component_response(self.url, query, headers, **kwargs)
+
     def dump_component_query(
         self,
         query: str,
@@ -527,4 +566,11 @@ class TokenizedComponentCrawler(ComponentCrawler):
             self.assets.path,
             asset_name,
             **kwargs,
+        )
+
+    def dump_component_response(
+        self, asset_name: dict[int, str] | str, response, **kwargs
+    ):
+        return dump_component_response(
+            self.assets.statuses, self.assets.path, asset_name, response, **kwargs
         )

dev/crawler/gateway.py
@@ -329,22 +329,46 @@ class GatewayCrawler(TokenizedComponentCrawler):
         # FIXME: download unknown node
 
     def _crawl_node_gpus_allocated(self, node):
-        self.dump_component_query(
-            f"/api/agents/{self.cluster}/node/{node}",
-            "node-with-gpus-allocated",
+        response = self.get_component_response(
+            f"/api/agents/{self.cluster}/node/{node}"
         )
+        print(response.json())
+        has_model = any(
+            [len(gres.split(":")) > 2 for gres in response.json()["gres"].split(",")]
+        )
+        if has_model:
+            asset = "node-with-gpus-model-allocated"
+        else:
+            asset = "node-with-gpus-allocated"
+        self.dump_component_response(asset, response)
 
     def _crawl_node_gpus_mixed(self, node):
-        self.dump_component_query(
-            f"/api/agents/{self.cluster}/node/{node}",
-            "node-with-gpus-mixed",
+        response = self.get_component_response(
+            f"/api/agents/{self.cluster}/node/{node}"
         )
+        print(response.json())
+        has_model = any(
+            [len(gres.split(":")) > 2 for gres in response.json()["gres"].split(",")]
+        )
+        if has_model:
+            asset = "node-with-gpus-model-mixed"
+        else:
+            asset = "node-with-gpus-mixed"
+        self.dump_component_response(asset, response)
 
     def _crawl_node_gpus_idle(self, node):
-        self.dump_component_query(
-            f"/api/agents/{self.cluster}/node/{node}",
-            "node-with-gpus-idle",
+        response = self.get_component_response(
+            f"/api/agents/{self.cluster}/node/{node}"
         )
+        print(response.json())
+        has_model = any(
+            [len(gres.split(":")) > 2 for gres in response.json()["gres"].split(",")]
+        )
+        if has_model:
+            asset = "node-with-gpus-model-idle"
+        else:
+            asset = "node-with-gpus-idle"
+        self.dump_component_response(asset, response)
 
     def _crawl_node_without_gpu(self, node):
         self.dump_component_query(

dev/crawler/slurmrestd.py
@@ -70,6 +70,14 @@ class SlurmrestdCrawler(ComponentCrawler):
         )
         self.auth = auth
 
+    def get_slurmrestd_json_response(
+        self, query: str, headers: dict[str, str] | None = None
+    ):
+        """Send GET HTTP request to slurmrestd and get JSON result, raw text
+        response, content type and status."""
+        text, content_type, status = self.cluster.query_slurmrestd(query, headers)
+        return json.loads(text), text, content_type, status
+
     def dump_slurmrestd_query(
         self,
         query: str,
@@ -86,7 +94,14 @@ class SlurmrestdCrawler(ComponentCrawler):
             return
 
         text, content_type, status = self.cluster.query_slurmrestd(query, headers)
+        self.dump_slurmrestd_response(
+            asset_name, text, content_type, status, limit_dump, limit_key
+        )
 
+    def dump_slurmrestd_response(
+        self, asset_name, text, content_type, status, limit_dump=0, limit_key=None
+    ):
+        """Save slurmrestd response asset."""
         if asset_name not in self.assets.statuses:
             self.assets.statuses[asset_name] = {}
         self.assets.statuses[asset_name]["content-type"] = content_type
@@ -385,22 +400,43 @@ class SlurmrestdCrawler(ComponentCrawler):
         )
 
     def _crawl_node_gpus_allocated(self, node):
-        self.dump_slurmrestd_query(
-            f"/slurm/v{self.cluster.api}/node/{node}",
-            "slurm-node-with-gpus-allocated",
+        _json, text, content_type, status = self.get_slurmrestd_json_response(
+            f"/slurm/v{self.cluster.api}/node/{node}"
         )
+        has_model = any(
+            [len(gres.split(":")) > 2 for gres in _json["nodes"][0]["gres"].split(",")]
+        )
+        if has_model:
+            asset = "slurm-node-with-gpus-model-allocated"
+        else:
+            asset = "slurm-node-with-gpus-allocated"
+        self.dump_slurmrestd_response(asset, text, content_type, status)
 
     def _crawl_node_gpus_mixed(self, node):
-        self.dump_slurmrestd_query(
-            f"/slurm/v{self.cluster.api}/node/{node}",
-            "slurm-node-with-gpus-mixed",
+        _json, text, content_type, status = self.get_slurmrestd_json_response(
+            f"/slurm/v{self.cluster.api}/node/{node}"
         )
+        has_model = any(
+            [len(gres.split(":")) > 2 for gres in _json["nodes"][0]["gres"].split(",")]
+        )
+        if has_model:
+            asset = "slurm-node-with-gpus-model-mixed"
+        else:
+            asset = "slurm-node-with-gpus-mixed"
+        self.dump_slurmrestd_response(asset, text, content_type, status)
 
     def _crawl_node_gpus_idle(self, node):
-        self.dump_slurmrestd_query(
-            f"/slurm/v{self.cluster.api}/node/{node}",
-            "slurm-node-with-gpus-idle",
+        _json, text, content_type, status = self.get_slurmrestd_json_response(
+            f"/slurm/v{self.cluster.api}/node/{node}"
         )
+        has_model = any(
+            [len(gres.split(":")) > 2 for gres in _json["nodes"][0]["gres"].split(",")]
+        )
+        if has_model:
+            asset = "slurm-node-with-gpus-model-idle"
+        else:
+            asset = "slurm-node-with-gpus-idle"
+        self.dump_slurmrestd_response(asset, text, content_type, status)
 
     def _crawl_node_without_gpu(self, node):
         self.dump_slurmrestd_query(

commit 5f6943112744e07673d64c5e8e6a04eda6dd03d9

chore(dev): fix regression in assets crawler
Small regression introduced in 822d95a.

dev/crawler/lib.py
@@ -465,7 +465,7 @@ def dump_component_query(
         response = requests.post(f"{url}{query}", headers=headers, **kwargs)
     else:
         raise RuntimeError(f"Unsupport request method {method}")
-    dump_component_response(
+    return dump_component_response(
         requests_statuses, assets_path, asset_name, response, prettify, limit_dump
     )
 