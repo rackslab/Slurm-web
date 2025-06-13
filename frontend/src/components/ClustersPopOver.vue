rackslab/Slurm-web

commit eac96025d82a1cf93a390423769429d20ac1d9d5

refactor(front): avoid mutating prop
While mutation of object props nested properties works, this is
considered a bad practice, it is spotted by eslint. Use emit for this
code pattern.

frontend/src/views/ClustersView.vue
@@ -120,7 +120,11 @@ onMounted(() => {
           role="list"
           class="divide-y divide-gray-100 overflow-hidden bg-white shadow-xs ring-1 ring-gray-900/5 lg:rounded-xl"
         >
-          <ClusterListItem v-for="cluster in clusters" :key="cluster.name" :cluster="cluster" />
+          <ClusterListItem
+            v-for="cluster in clusters"
+            :key="cluster.name"
+            :cluster-name="cluster.name"
+          />
         </ul>
       </div>
     </section>

frontend/src/components/clusters/ClustersListItem.vue
@@ -10,16 +10,17 @@ import { onMounted, ref } from 'vue'
 import { RouterLink, useRouter } from 'vue-router'
 import { useRuntimeStore } from '@/stores/runtime'
 import { useGatewayAPI } from '@/composables/GatewayAPI'
-import type { ClusterDescription } from '@/composables/GatewayAPI'
+import type { ClusterDescription, ClusterStats } from '@/composables/GatewayAPI'
 import { AuthenticationError } from '@/composables/HTTPErrors'
 import { ChevronRightIcon } from '@heroicons/vue/20/solid'
 import { TagIcon } from '@heroicons/vue/20/solid'
 import { ServerIcon, PlayCircleIcon } from '@heroicons/vue/24/outline'
 import LoadingSpinner from '@/components/LoadingSpinner.vue'
 
-const { cluster } = defineProps<{ cluster: ClusterDescription }>()
+const { clusterName } = defineProps<{ clusterName: string }>()
 
 const runtimeStore = useRuntimeStore()
+const cluster = runtimeStore.getCluster(clusterName)
 const loading = ref<boolean>(true)
 
 function reportAuthenticationError(error: AuthenticationError) {

commit 63c1ba3101f7da09caa323ad023f74dcad06d964

fix(front): handle denied clusters in list
Fix regression introduced in eac9602 in handling of denied clusters in
clusters list. All available clusters are now added to runtime store
availableClusters list, whatever the permissions on these clusters. A
method getAllowedClusters() is added for components that needs to have
the clusters with permissions only.

frontend/src/components/ClustersPopOver.vue
@@ -20,7 +20,7 @@ const runtimeStore = useRuntimeStore()
 </script>
 
 <template>
-  <template v-if="runtimeStore.availableClusters.length > 1">
+  <template v-if="runtimeStore.getAllowedClusters().length > 1">
     <Popover class="relative">
       <PopoverButton
         class="hover:bg-slurmweb-light hover:dark:bg-slurmweb-dark/30 inline-flex items-center gap-x-1 rounded-sm p-3 leading-6 font-bold text-transparent hover:text-gray-900 focus:outline-hidden hover:dark:text-gray-200"
@@ -46,7 +46,7 @@ const runtimeStore = useRuntimeStore()
           >
             <div class="p-4">
               <div
-                v-for="cluster in runtimeStore.availableClusters"
+                v-for="cluster in runtimeStore.getAllowedClusters()"
                 :key="cluster.name"
                 class="group relative flex gap-x-6 rounded-lg p-4 hover:bg-gray-50 hover:dark:bg-gray-800"
               >

frontend/src/stores/runtime.ts
@@ -74,6 +74,10 @@ export const useRuntimeStore = defineStore('runtime', () => {
     return availableClusters.value.filter((cluster) => cluster.name === name)[0]
   }
 
+  function getAllowedClusters() {
+    return availableClusters.value.filter((cluster) => cluster.permissions.actions.length > 0)
+  }
+
   function checkClusterAvailable(name: string): boolean {
     return availableClusters.value.filter((cluster) => cluster.name === name).length > 0
   }
@@ -126,6 +130,7 @@ export const useRuntimeStore = defineStore('runtime', () => {
     currentCluster,
     addCluster,
     getCluster,
+    getAllowedClusters,
     checkClusterAvailable,
     hasPermission,
     hasClusterPermission,

frontend/src/views/ClustersView.vue
@@ -46,9 +46,7 @@ async function getClustersDescriptions() {
        * set to true if stats retrieval fail later on.
        */
       element.error = false
-      if (element.permissions.actions.length > 0) {
-        runtimeStore.addCluster(element)
-      }
+      runtimeStore.addCluster(element)
     })
     loaded.value = true
   } catch (error) {

frontend/src/views/settings/SettingsAccount.vue
@@ -58,7 +58,7 @@ const authStore = useAuthStore()
     <div class="mt-6 border-t border-gray-100 dark:border-gray-700">
       <dl class="divide-y divide-gray-100 dark:divide-gray-700">
         <div
-          v-for="cluster in runtimeStore.availableClusters"
+          v-for="cluster in runtimeStore.getAllowedClusters()"
           :key="cluster.name"
           class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0"
         >