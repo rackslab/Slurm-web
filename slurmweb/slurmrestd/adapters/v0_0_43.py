834b93e851c925b4f822b35bc468765a607fcd97
Rémi Palancher <remi@rackslab.io>
2025-11-13 15:51:33 +0100

    refactor(agent): adapter for v0.0.43

--- slurmweb/slurmrestd/adapters/v0_0_43.py
+++ slurmweb/slurmrestd/adapters/v0_0_43.py
@@ -4,12 +4,31 @@
 #
 # SPDX-License-Identifier: MIT
 
+import logging
 from .base import BaseAdapter
+
+
+logger = logging.getLogger(__name__)
 
 
 class AdapterV0_0_43(BaseAdapter):
     """Adapter from API version 0.0.43 to 0.0.44.
 
-    Based on asset comparison, no differences found between 0.0.43 and 0.0.44
-    in endpoints called by Slurm-web. This adapter is a placeholder for future changes.
+    Differences spotted between v0.0.43 and v0.0.44 API but ignored:
+
+    GET /slurm/v{version}/job/{job_id}
+      + responses.200.properties.jobs.items.properties.step_id
+        → not used by Slurm-web
+      + responses.200.properties.jobs.items.properties.submit_line
+        → not used by Slurm-web (obtained from slurmdb/job/{job_id})
+
+    GET /slurm/v{version}/jobs/
+      + responses.200.properties.jobs.items.properties.step_id
+        → not used by Slurm-web
+      + responses.200.properties.jobs.items.properties.submit_line
+        → not used by Slurm-web
+
+    GET /slurmdb/v{version}/jobs/
+      + parameters.job_altered
+        → not used by Slurm-web
     """
