407a04af14c8a333c3373d16a66d2fd8db196164
Rémi Palancher <remi@rackslab.io>
2025-11-17 15:47:54 +0100

    tests(front): update clusters list components
    Update tests of components used by clusters list view to cover new
    ClusterStats component and check ping call in ClustersListItem
    component.

--- frontend/tests/components/clusters/ClustersListItem.spec.ts
+++ frontend/tests/components/clusters/ClustersListItem.spec.ts
@@ -1,15 +1,15 @@
 import { describe, test, expect, beforeEach, vi } from 'vitest'
 import { flushPromises, shallowMount } from '@vue/test-utils'
 import ClusterListItem from '@/components/clusters/ClustersListItem.vue'
-import stats from '../../assets/stats.json'
+import ClusterStats from '@/components/clusters/ClusterStats.vue'
+import ping from '../../assets/ping.json'
 import { init_plugins } from '../../lib/common'
 import { useRuntimeStore } from '@/stores/runtime'
 import { APIServerError, AuthenticationError } from '@/composables/HTTPErrors'
 import { useAuthStore } from '@/stores/auth'
-import LoadingSpinner from '@/components/LoadingSpinner.vue'
 
 const mockGatewayAPI = {
-  stats: vi.fn()
+  ping: vi.fn()
 }
 
 vi.mock('@/composables/GatewayAPI', () => ({
@@ -33,11 +33,11 @@ describe('ClustersListItem.vue', () => {
         cache: true
       }
     ]
-    mockGatewayAPI.stats.mockReset()
+    mockGatewayAPI.ping.mockReset()
   })
   test('cluster with permission', async () => {
     useRuntimeStore().availableClusters[0].permissions.actions = ['view-stats', 'view-jobs']
-    mockGatewayAPI.stats.mockReturnValueOnce(Promise.resolve(stats))
+    mockGatewayAPI.ping.mockReturnValueOnce(Promise.resolve(ping))
     const wrapper = shallowMount(ClusterListItem, {
       props: {
         clusterName: useRuntimeStore().availableClusters[0].name
@@ -46,13 +46,11 @@ describe('ClustersListItem.vue', () => {
     // Wait for result of clusters requests
     await flushPromises()
     // assert stats have been retrieved
-    expect(mockGatewayAPI.stats).toBeCalled()
+    expect(mockGatewayAPI.ping).toBeCalled()
     // Check presence of Slurm version
     expect(wrapper.get('span span').text()).toContain('Slurm ')
-    // Check presence of nodes/jobs stats
-    const statsElements = wrapper.findAll('p')
-    expect(statsElements[0].text()).toContain('nodes')
-    expect(statsElements[1].text()).toContain('job')
+    // Check presence of cluster stats component
+    wrapper.getComponent(ClusterStats)
     // Check cluster status is available
     expect(wrapper.get('div div p').text()).toBe('Available')
     // Check error flag is false
@@ -60,7 +58,7 @@ describe('ClustersListItem.vue', () => {
   })
   test('cluster error', async () => {
     useRuntimeStore().availableClusters[0].permissions.actions = ['view-stats', 'view-jobs']
-    mockGatewayAPI.stats.mockImplementationOnce(() => {
+    mockGatewayAPI.ping.mockImplementationOnce(() => {
       throw new APIServerError(500, 'fake error')
     })
     const wrapper = shallowMount(ClusterListItem, {
@@ -70,12 +68,12 @@ describe('ClustersListItem.vue', () => {
     })
     // Wait for result of clusters requests
     await flushPromises()
-    // assert stats have been retrieved
-    expect(mockGatewayAPI.stats).toBeCalled()
+    // assert ping have been retrieved
+    expect(mockGatewayAPI.ping).toBeCalled()
     // Check Slurm version is absent
     expect(wrapper.find('span span').exists()).toBeFalsy()
-    // Check there is only one paragraph for cluster status, not for stats
-    expect(wrapper.findAll('p').length).toBe(1)
+    // Check absence of cluster stats component
+    expect(wrapper.findComponent(ClusterStats).exists()).toBeFalsy()
     // Check cluster status informs about ongoing issue
     expect(wrapper.get('div div p').text()).toBe('Ongoing issue')
     // Check error flag is true
@@ -83,21 +81,21 @@ describe('ClustersListItem.vue', () => {
   })
   test('cluster loading', async () => {
     useRuntimeStore().availableClusters[0].permissions.actions = ['view-stats', 'view-jobs']
-    mockGatewayAPI.stats.mockReturnValueOnce(Promise.resolve(stats))
+    mockGatewayAPI.ping.mockReturnValueOnce(Promise.resolve(ping))
     const wrapper = shallowMount(ClusterListItem, {
       props: {
         clusterName: useRuntimeStore().availableClusters[0].name
       }
     })
-    // assert stats are being retrieved
-    expect(mockGatewayAPI.stats).toBeCalled()
-    // Check presence of loading spinner
-    expect(wrapper.findComponent(LoadingSpinner).exists()).toBeTruthy()
+    // assert ping is being retrieved
+    expect(mockGatewayAPI.ping).toBeCalled()
+    // Check absence of cluster stats component
+    expect(wrapper.findComponent(ClusterStats).exists()).toBeFalsy()
     // Check cluster status is loading
     expect(wrapper.get('div div p').text()).toBe('Loading')
   })
   test('cluster without permission', async () => {
-    mockGatewayAPI.stats.mockReturnValueOnce(Promise.resolve(stats))
+    mockGatewayAPI.ping.mockReturnValueOnce(Promise.resolve(ping))
     const wrapper = shallowMount(ClusterListItem, {
       props: {
         clusterName: useRuntimeStore().availableClusters[0].name
@@ -105,33 +103,36 @@ describe('ClustersListItem.vue', () => {
     })
     // Wait for result of clusters requests
     await flushPromises()
-    // assert stats not retrieved
-    expect(mockGatewayAPI.stats).not.toBeCalled()
+    // assert ping not retrieved
+    expect(mockGatewayAPI.ping).not.toBeCalled()
     // Check Slurm version is absent
     expect(wrapper.find('span span').exists()).toBeFalsy()
-    // Check there is only one paragraph for cluster status, not for stats
-    expect(wrapper.findAll('p').length).toBe(1)
+    // Check absence of cluster stats component
+    expect(wrapper.findComponent(ClusterStats).exists()).toBeFalsy()
+    // Check cluster status is denied
     expect(wrapper.get('div div p').text()).toBe('Denied')
   })
   test('cluster without view-stats permission', async () => {
     useRuntimeStore().availableClusters[0].permissions.actions = ['view-jobs']
+    mockGatewayAPI.ping.mockReturnValueOnce(Promise.resolve(ping))
     const wrapper = shallowMount(ClusterListItem, {
       props: {
         clusterName: useRuntimeStore().availableClusters[0].name
       }
     })
     await flushPromises()
-    // assert stats not retrieved
-    expect(mockGatewayAPI.stats).not.toBeCalled()
-    // Check Slurm version is absent
-    expect(wrapper.find('span span').exists()).toBeFalsy()
-    // Check there is only one paragraph for cluster status, not for stats
-    expect(wrapper.findAll('p').length).toBe(1)
+    // assert ping have been retrieved
+    expect(mockGatewayAPI.ping).toBeCalled()
+    // Check presence of Slurm version
+    expect(wrapper.get('span span').text()).toContain('Slurm ')
+    // Check absence of cluster stats component
+    expect(wrapper.findComponent(ClusterStats).exists()).toBeFalsy()
+    // Check cluster status is available
     expect(wrapper.get('div div p').text()).toBe('Available')
   })
   test('authentication error', async () => {
     useRuntimeStore().availableClusters[0].permissions.actions = ['view-stats', 'view-jobs']
-    mockGatewayAPI.stats.mockImplementationOnce(() => {
+    mockGatewayAPI.ping.mockImplementationOnce(() => {
       throw new AuthenticationError('fake authentication error')
     })
     shallowMount(ClusterListItem, {
@@ -139,7 +140,7 @@ describe('ClustersListItem.vue', () => {
         clusterName: useRuntimeStore().availableClusters[0].name
       }
     })
-    // Wait for result of stats requests
+    // Wait for result of ping requests
     await flushPromises()
     // Check that returnUrl was set to current route
     expect(useAuthStore().returnUrl).toBe('/clusters')
