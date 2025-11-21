5a38a2d2ddd97af6acae13bbe42b879ed2424dfc
Rémi Palancher <remi@rackslab.io>
2025-11-21 10:17:24 +0100

    tests(front): load app config as function

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -3,7 +3,7 @@ import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
 import viteConfig from './vite.config'
 
 export default mergeConfig(
-  viteConfig,
+  viteConfig({ mode: 'test', command: 'serve' }),
   defineConfig({
     test: {
       environment: 'jsdom',
